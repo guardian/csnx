6edaa7078b016518d371c6791ab9ef72a65cc1c3
Alex Sanders <alex@sndrs.dev>
2022-08-04 11:55:51 +0100

    WIP make tsconfig buildable and build packages to local `dist` dir
    Co-authored-by: Joe Cowton <joecowton1@users.noreply.github.com>

---
+++ libs/tsconfig/index.ts
@@ -0,0 +1,3 @@
+// this is a placeholder to keep @nrwl/js:tsc happy
+// it has no other purpose
+// we hardcode the `main` export in our package.json
---
+++ libs/x/README.md
@@ -0,0 +1,7 @@
+# x
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build x` to build the library.
