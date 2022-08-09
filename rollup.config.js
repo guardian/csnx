36bb8b8dabcce76e234d08d501f9e744b6949d9f
Joe Cowton <joe.cowton@guardian.co.uk>
2022-08-09 16:05:07 +0100

    Remove unused config

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-const json = require('@rollup/plugin-json');
-const commonjs = require('@rollup/plugin-commonjs');
-
-module.exports = (config) => {
-	return {
-		...config,
-		plugins: [...config.plugins, commonjs(), json()],
-	};
-};
