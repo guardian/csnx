e92f61a4c54834716dee2c2d0f46ca06c7df96d0
Joe Cowton <joe.cowton@guardian.co.uk>
2022-08-10 16:24:00 +0100

    Create rollup config

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const ts = require('rollup-plugin-ts');
+const copy = require('rollup-plugin-copy');
+
+const external = ['@guardian/a'];
+
+module.exports = (_, options) => ({
+	input: options.entryFile,
+	output: [
+		{
+			dir: `${options.outputPath}/cjs`,
+			format: 'cjs',
+			sourcemap: true,
+			preserveModules: true,
+		},
+		{
+			dir: `${options.outputPath}/esm`,
+			format: 'es',
+			sourcemap: true,
+			preserveModules: true,
+		},
+	],
+	plugins: [
+		copy({
+			targets: options.assets.map((asset) => ({
+				src: [`${asset.input}/${asset.glob}`],
+				dest: `${options.outputPath}`,
+			})),
+		}),
+		nodeResolve({
+			extensions: ['.ts', '.tsx', '.mjs', '.jsx', '.js'],
+		}),
+		ts({ tsconfig: options.tsconfig }),
+		commonjs(),
+	],
+	external,
+});
