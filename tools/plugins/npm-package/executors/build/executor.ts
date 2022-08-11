d5140b7201a8847f7a585d40b496ecf4e27c8ab3
Alex Sanders <alex@sndrs.dev>
2022-08-11 10:42:46 +0100

    add rollup executor code to skeleton

--- tools/plugins/npm-package/executors/build/executor.ts
+++ tools/plugins/npm-package/executors/build/executor.ts
@@ -1,15 +1,66 @@
 import type { ExecutorContext } from '@nrwl/devkit';
+import { rollup } from 'rollup';
+import commonjs from '@rollup/plugin-commonjs';
+import ts from 'rollup-plugin-ts';
 import { logger } from '@nrwl/devkit';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import type { InputOption } from 'rollup';
+import util from 'node:util';
+import childProcess from 'node:child_process';
+import cpy from 'cpy';
+
+const exec = util.promisify(childProcess.exec);
 
 export interface BuildExecutorOptions {
-	textToEcho: string;
+	main: InputOption;
+	outputPath: string;
+	tsConfig: string;
+	packageJson: string;
+	assets: Array<string>;
 }
+
+const getConfig = (options, format) => ({
+	output: {
+		dir: `${options.outputPath}/${format}`,
+		format,
+		sourcemap: true,
+		preserveModules: true,
+	},
+	plugins: [
+		nodeResolve({
+			extensions: ['.ts', '.tsx', '.mjs', '.jsx', '.js'],
+		}),
+		ts({ tsconfig: options.tsConfig }),
+		commonjs(),
+	],
+});
 
 export default async function buildExecutor(
 	options: BuildExecutorOptions,
 	context: ExecutorContext,
 ): Promise<{ success: boolean }> {
-	logger.info(options);
+	try {
+		// remove old build
+		await exec(`rm -rf ${options.outputPath}`);
 
-	return { success: true };
+		// copy assets over
+		await cpy([options.packageJson, ...options.assets], options.outputPath, {
+			cwd: context.root,
+		});
+
+		// create build for each module type
+		await Promise.all(
+			['cjs', 'esm'].map(async (format) => {
+				const { plugins, output } = getConfig(options, format);
+				const bundle = await rollup({ input: options.main, plugins });
+				await bundle.write(output);
+				return bundle.close();
+			}),
+		);
+
+		return { success: true };
+	} catch (e) {
+		logger.error(e);
+		return { success: false };
+	}
 }
