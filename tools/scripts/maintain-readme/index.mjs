948d2d3e5e72efa0ddef01c770904ef2e615160f
Alex Sanders <alex@sndrs.dev>
2022-08-13 15:52:01 +0100

    break update readme scripts out a bit

---
+++ tools/scripts/maintain-readme/index.mjs
@@ -0,0 +1,21 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { updateReadmeSection } from './utils/update-readme-section.mjs';
+import { pathFromRoot, projectRoot } from '../project-paths.mjs';
+import { getPackageList } from './get-package-list.mjs';
+
+const thisFilePath = fileURLToPath(import.meta.url);
+const thisFilePathFromRoot = pathFromRoot(thisFilePath);
+
+const readMePath = path.resolve(projectRoot, 'README.md');
+let readme = await readFile(readMePath, 'utf8');
+
+readme = updateReadmeSection({
+	readme,
+	label: 'PACKAGES',
+	updates: await getPackageList(),
+	updater: thisFilePathFromRoot,
+});
+
+await writeFile(readMePath, readme);
