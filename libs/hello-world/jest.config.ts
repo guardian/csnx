5eda800af28582bfe17b92926900c54232e29dd1
Alex Sanders <alex@sndrs.dev>
2022-08-17 15:15:50 +0100

    add temp `@csnx/hello-world` lib

---
+++ libs/hello-world/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-default-export -- that's what jest likes */
+export default {
+	displayName: 'hello-world',
+	preset: '../../jest.preset.js',
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+		},
+	},
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]sx?$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+	coverageDirectory: '../../coverage/libs/hello-world',
+};
