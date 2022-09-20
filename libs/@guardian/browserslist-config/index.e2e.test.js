a7c39ec431950f166f9b6ec8acd356cb9e780953
Alex Sanders <alex@sndrs.dev>
2022-09-20 10:30:49 +0100

    add `browserslist-config` e2e tests

---
+++ libs/@guardian/browserslist-config/index.e2e.test.js
@@ -0,0 +1,8 @@
+const srcExports = require('./browserslist');
+const bundleExports = require('../../../dist/libs/@guardian/browserslist-config');
+
+describe('The bundle', () => {
+	it('exports everything it should', () => {
+		expect(srcExports).toEqual(bundleExports);
+	});
+});
---
+++ libs/@guardian/browserslist-config/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	displayName: '@guardian/browserslist-config',
+	preset: '../../../jest.preset.js',
+	testEnvironment: 'node',
+	moduleFileExtensions: ['js', 'jsx'],
+	coverageDirectory: '../../../coverage/libs/@guardian/browserslist-config',
+};
