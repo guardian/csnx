cca82a58c86ef94b75799ce2840cfb12bf032ba3
Alex Sanders <alex@sndrs.dev>
2022-09-20 10:14:57 +0100

    add e2e test for `eslint-config-typescript`

---
+++ libs/@guardian/eslint-config-typescript/index.e2e.test.js
@@ -0,0 +1,8 @@
+const srcExports = require('./index');
+const bundleExports = require('../../../dist/libs/@guardian/eslint-config-typescript');
+
+describe('The bundle', () => {
+	it('exports everything it should', () => {
+		expect(srcExports).toEqual(bundleExports);
+	});
+});
