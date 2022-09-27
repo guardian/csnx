dee71d97f05a94ef525f13fe02c60277493bddad
Alex Sanders <alex@sndrs.dev>
2022-09-27 10:15:17 +0100

    convert our npm packager to wrap `@nrwl/rollup:rollup`
    # Conflicts:
    #	pnpm-lock.yaml

---
+++ tools/nx-plugins/npm-package/executors/compile/executor.ts
@@ -0,0 +1,40 @@
+import path from 'node:path';
+import type { ExecutorContext } from '@nrwl/devkit';
+import { logger } from '@nrwl/devkit';
+import { rollupExecutor } from '@nrwl/rollup';
+import type { RollupExecutorOptions } from '@nrwl/rollup/src/executors/rollup/schema';
+import { setPkgDefaults } from '../utils/set-pkg-defaults';
+
+/**
+ * Compiles source code into an NPM package for publishing.
+ * Wraps @nrwl/rollup:rollup executor with some defaults set by us.
+ */
+export default async function (
+	options: RollupExecutorOptions,
+	context: ExecutorContext,
+) {
+	try {
+		// get some default from a rollup config
+		// it's a bit weird we have to do this, but this is how the original executor works
+		options.rollupConfig = path.resolve(__dirname, 'rollup.config.js');
+
+		// set some defaults
+		options.updateBuildableProjectDepsInPackageJson = true;
+		options.format = ['esm', 'cjs'];
+		options.generateExportsField = true;
+		options.compiler = 'tsc';
+
+		// we need to wait for the promises returned from the generator to resolve
+		// https://github.com/nrwl/nx/blob/master/packages/rollup/src/executors/rollup/rollup.impl.ts
+		for await (const i of rollupExecutor(options, context)) {
+			void i;
+		}
+
+		await setPkgDefaults(options);
+
+		return { success: true };
+	} catch (e) {
+		logger.fatal(e);
+		return { success: false };
+	}
+}
---
+++ tools/nx-plugins/npm-package/executors/utils/set-pkg-defaults.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-var-requires -- we're loading json files for NPM */
+import { writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { isObject } from '@guardian/libs';
+import type { RollupExecutorOptions } from '@nrwl/rollup/src/executors/rollup/schema';
+import sortPkgJson from 'sort-package-json';
+import type { PackOptions } from '../executors/pack/executor';
+
+/**
+ * Sets some useful package default fields, and removes unnecessary ones
+ */
+export const setPkgDefaults = async (
+	options: PackOptions | RollupExecutorOptions,
+) => {
+	const projectPath = path.dirname(options.project);
+	const pkgPath = path.resolve(options.outputPath, 'package.json');
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- we test it manually
+	const pkg = require(pkgPath);
+
+	if (isObject(pkg)) {
+		delete pkg.pnpm;
+		delete pkg.packageManager;
+		delete pkg.scripts;
+		delete pkg.devDependencies;
+
+		pkg.license = 'MIT';
+		pkg.readme = 'README.md';
+		pkg.homepage = `https://github.com/guardian/csnx/tree/main/${projectPath}#readme`;
+		pkg.bugs = {
+			url: 'https://github.com/guardian/csnx/issues',
+		};
+		pkg.repository = {
+			type: 'git',
+			url: 'git+https://github.com/guardian/csnx.git',
+			directory: projectPath,
+		};
+
+		if (!pkg.private) {
+			pkg.publishConfig = {
+				access: 'public',
+			};
+		}
+
+		await writeFile(pkgPath, JSON.stringify(sortPkgJson(pkg), null, 2));
+	}
+};
