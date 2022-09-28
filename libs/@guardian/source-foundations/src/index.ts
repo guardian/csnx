3f7cccbe13ed36a13c5c0ada20a6409ad728417b
Alex Sanders <alex@sndrs.dev>
2022-09-28 11:29:40 +0100

    add comments about importing source

--- libs/@guardian/source-foundations/src/index.ts
+++ libs/@guardian/source-foundations/src/index.ts
@@ -1,2 +1,5 @@
+// this is a placeholder file as we prep for importing source to csnx.
+// source itself still live in https://github.com/guardian/source
+
 export type Placeholder = () => string;
 export const placeholder: Placeholder = () => 'nothing to see here';
