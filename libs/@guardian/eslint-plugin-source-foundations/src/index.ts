2198d16fb8c1e3ac391de34823d179514fefca56
TJ Silver <thalia.silver@guardian.co.uk>
2022-10-05 11:30:37 +0100

    replace src in foundations plugin with placeholder code

--- libs/@guardian/eslint-plugin-source-foundations/src/index.ts
+++ libs/@guardian/eslint-plugin-source-foundations/src/index.ts
@@ -1,19 +1,5 @@
-import { noStarImportsOrExports } from './rules/no-star-imports-or-exports';
-import { validFoundationsImportPath } from './rules/valid-import-path';
+// this is a placeholder file as we prep for importing source to csnx.
+// source itself still live in https://github.com/guardian/source
 
-export const rules = {
-	'valid-foundations-import-path': validFoundationsImportPath,
-	'no-star-imports-or-exports': noStarImportsOrExports,
-};
-
-export const configs = {
-	recommended: {
-		plugins: ['@guardian/source-foundations', 'import'],
-
-		rules: {
-			'@guardian/source-foundations/valid-foundations-import-path': 'error',
-			'@guardian/source-foundations/no-star-imports-or-exports': 'error',
-			'import/no-duplicates': 'error',
-		},
-	},
-};
+export type Placeholder = () => string;
+export const placeholder: Placeholder = () => 'nothing to see here';
