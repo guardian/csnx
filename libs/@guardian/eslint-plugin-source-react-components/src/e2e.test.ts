8a5719acc00c6c5d65541a1c245fd299059f4a11
TJ Silver <thalia.silver@guardian.co.uk>
2022-10-07 12:35:02 +0100

    Revert "add e2e test for components plugin"
    This reverts commit e13d7ff9ed80620470350e7ebd8aff6dd55e3d15.

--- libs/@guardian/eslint-plugin-source-react-components/src/e2e.test.ts
+++
@@ -1,8 +0,0 @@
-import * as bundleExports from '../../../../dist/libs/@guardian/eslint-plugin-source-react-components/cjs';
-import * as srcExports from '.';
-
-describe('The bundle', () => {
-	it('exports everything it should', () => {
-		expect(srcExports).toEqual(bundleExports);
-	});
-});
