2af0cf042815227bc201223d1883458fe1514b62
TJ Silver <thalia.silver@guardian.co.uk>
2022-10-07 12:35:39 +0100

    Revert "add e2e test for foundations plugin"
    This reverts commit 040408126f35426f7a23c1729718e9f25765a6f3.

--- libs/@guardian/eslint-plugin-source-foundations/src/e2e.test.ts
+++
@@ -1,8 +0,0 @@
-import * as bundleExports from '../../../../dist/libs/@guardian/eslint-plugin-source-foundations/cjs';
-import * as srcExports from '.';
-
-describe('The bundle', () => {
-	it('exports everything it should', () => {
-		expect(srcExports).toEqual(bundleExports);
-	});
-});
