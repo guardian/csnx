401e1a5e150f732a246b781838b3a63a80af7872
Alex Sanders <alex@sndrs.dev>
2022-10-21 17:44:15 +0100

    Create `@guardian/core-web-vitals`

--- libs/@guardian/core-web-vitals/jest.e2e.setup.js
+++ libs/@guardian/core-web-vitals/jest.e2e.setup.js
@@ -1,6 +1,6 @@
 // Mock `./src/index` with whatever `package.json` points at in dist.
 // This means we can run the unit tests against `dist` instead.
 
-const dist = require('../../../dist/libs/@guardian/core-web-vitals/cjs');
+const dist = require('../../../dist/libs/@guardian/core-web-vitals');
 
 jest.mock('./src/index', () => dist);
