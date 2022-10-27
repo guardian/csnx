884e945fe535c3e96dc0af480fa039aa45e4e73e
Alex Sanders <alex@sndrs.dev>
2022-10-27 16:02:18 +0100

    build prod version of composed storybook

--- apps/csnx/.storybook/main.js
+++ apps/csnx/.storybook/main.js
@@ -17,18 +17,36 @@ module.exports = {
 
 		return config;
 	},
-	refs: {
-		'source-foundations': {
-			title: 'source-foundations',
-			url: 'http://localhost:4401',
-		},
-		'source-react-components': {
-			title: 'source-react-components',
-			url: 'http://localhost:4402',
-		},
-		'source-react-components-development-kitchen': {
-			title: 'source-react-components-development-kitchen',
-			url: 'http://localhost:4403',
-		},
+	refs: (config, { configType }) => {
+		if (configType === 'DEVELOPMENT') {
+			return {
+				'source-foundations': {
+					title: 'source-foundations',
+					url: 'http://localhost:4401',
+				},
+				'source-react-components': {
+					title: 'source-react-components',
+					url: 'http://localhost:4402',
+				},
+				'source-react-components-development-kitchen': {
+					title: 'source-react-components-development-kitchen',
+					url: 'http://localhost:4403',
+				},
+			};
+		}
+		return {
+			'source-foundations': {
+				title: 'source-foundations',
+				url: 'https://main--635a6aa8070dfdbfdac36f65.chromatic.com',
+			},
+			'source-react-components': {
+				title: 'source-react-components',
+				url: 'https://main--635a6fffacd30d393597c1ff.chromatic.com',
+			},
+			'source-react-components-development-kitchen': {
+				title: 'source-react-components-development-kitchen',
+				url: 'https://main--635a7057acd30d393597c208.chromatic.com',
+			},
+		};
 	},
 };
