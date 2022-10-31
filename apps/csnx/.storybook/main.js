5a80490371b81aa703d5bbce237d285e5c8cefa3
Alex Sanders <alex@sndrs.dev>
2022-10-31 20:27:54 +0000

    use latest cloudfronts

--- apps/csnx/.storybook/main.js
+++ apps/csnx/.storybook/main.js
@@ -37,15 +37,15 @@ module.exports = {
 		return {
 			'source-foundations': {
 				title: 'source-foundations',
-				url: 'https://d3i1l5r238v6d6.cloudfront.net',
+				url: 'https://dkueb6x1zvheb.cloudfront.net',
 			},
 			'source-react-components': {
 				title: 'source-react-components',
-				url: 'https://d1qsfuwwsg3os7.cloudfront.net',
+				url: 'https://d2fsrka4th9xxh.cloudfront.net',
 			},
 			'source-react-components-development-kitchen': {
 				title: 'source-react-components-development-kitchen',
-				url: 'https://d1ew75kvll6wv9.cloudfront.net',
+				url: 'https://d3ag6334pu7s8o.cloudfront.net',
 			},
 		};
 	},
