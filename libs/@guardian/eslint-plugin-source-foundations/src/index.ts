61aaccc3d593e5a11fd953cd0d7193b98c8f23ce
Oliver Barnwell <barnwelloliver@gmail.com>
2022-11-03 10:45:07 +0000

    initial import of eslint-plugin-source-foundations and eslint-plugin-source-react-components

--- libs/@guardian/eslint-plugin-source-foundations/src/index.ts
+++ libs/@guardian/eslint-plugin-source-foundations/src/index.ts
@@ -1,5 +1,19 @@
-// this is a placeholder file as we prep for importing source to csnx.
-// source itself still live in https://github.com/guardian/source
+import { noStarImportsOrExports } from './rules/no-star-imports-or-exports';
+import { validFoundationsImportPath } from './rules/valid-import-path';
 
-export type Placeholder = () => string;
-export const placeholder: Placeholder = () => 'nothing to see here';
+export const rules = {
+	'valid-foundations-import-path': validFoundationsImportPath,
+	'no-star-imports-or-exports': noStarImportsOrExports,
+};
+
+export const configs = {
+	recommended: {
+		plugins: ['@guardian/source-foundations', 'import'],
+
+		rules: {
+			'@guardian/source-foundations/valid-foundations-import-path': 'error',
+			'@guardian/source-foundations/no-star-imports-or-exports': 'error',
+			'import/no-duplicates': 'error',
+		},
+	},
+};
