9b21be70890ec54fbdfab02aa94e061ca59465f6
Oliver Barnwell <barnwelloliver@gmail.com>
2022-11-07 11:56:07 +0000

    add matcher to setup files after env

--- libs/@guardian/source-foundations/jest.config.ts
+++ libs/@guardian/source-foundations/jest.config.ts
@@ -13,4 +13,5 @@ export default {
 	},
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 	coverageDirectory: '../../../coverage/libs/@guardian/source-foundations',
+	setupFilesAfterEnv: ['./lib/jest-matchers/toBeValidCSS.ts'],
 };
