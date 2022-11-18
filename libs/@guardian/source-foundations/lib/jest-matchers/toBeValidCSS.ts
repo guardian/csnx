76ad51a672925604255eaa5b1e18481d1000ae55
Oliver Barnwell <barnwelloliver@gmail.com>
2022-11-18 11:28:12 +0000

    fix type checking for jest matchers, and ensure that they are registered for source-foundations

--- libs/@guardian/source-foundations/lib/jest-matchers/toBeValidCSS.ts
+++ libs/@guardian/source-foundations/lib/jest-matchers/toBeValidCSS.ts
@@ -1,22 +1,6 @@
 import type { Warning } from 'lightningcss';
 import { transform } from 'lightningcss';
 
-type ToBeValidCSSOptions = {
-	/**
-	 * Set this to true if the CSS is a fragment (e.g. not wrapped in a selector and valid on its own)
-	 */
-	isFragment?: boolean;
-};
-
-declare global {
-	// eslint-disable-next-line @typescript-eslint/no-namespace -- we extend the jest matchers interface like this
-	namespace jest {
-		interface Matchers<R> {
-			toBeValidCSS(options?: ToBeValidCSSOptions): R;
-		}
-	}
-}
-
 expect.extend({
 	/**
 	 * Uses the lightningcss library to validate given CSS
