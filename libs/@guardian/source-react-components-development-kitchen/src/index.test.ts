a03b90682c6dcd1782ee7c7ea6376b5f06f9ef72
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2022-12-07 12:23:04 +0000

    export AgeWarning component

--- libs/@guardian/source-react-components-development-kitchen/src/index.test.ts
+++ libs/@guardian/source-react-components-development-kitchen/src/index.test.ts
@@ -3,6 +3,7 @@ import * as pkgExports from './index';
 // this makes sure no type exports have been removed
 // it won't catch that new ones have been added, but can anyone?
 export type {
+	AgeWarningProps,
 	EditorialButtonProps,
 	EditorialLinkButtonProps,
 	ExpandingWrapperProps,
@@ -22,6 +23,7 @@ export type {
 
 it('Should have exactly these exports', () => {
 	expect(Object.keys(pkgExports).sort()).toEqual([
+		'AgeWarning',
 		'DashedLines',
 		'Divider',
 		'DottedLines',
