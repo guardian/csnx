2cd51a5bb61a5c72aead1cb07291d16eb79c6678
Sophie <sophiemacmillan93@gmail.com>
2023-01-16 12:29:22 +0000

    Tweak expanding wrapper styling, export the default themes and allow cssOverrides

--- libs/@guardian/source-react-components-development-kitchen/src/expanding-wrapper/types.ts
+++ libs/@guardian/source-react-components-development-kitchen/src/expanding-wrapper/types.ts
@@ -1,4 +1,5 @@
 import type { ReactElement } from 'react';
+import type { SerializedStyles } from '@emotion/react';
 
 export interface ExpandingWrapperProps {
 	children: ReactElement;
@@ -6,6 +7,10 @@ export interface ExpandingWrapperProps {
 	disableTabbingWhenCollapsed?: boolean;
 	renderExtra?: () => ReactElement;
 	expandCallback?: (expanded: boolean) => void;
+	/**
+	 * Override component styles by passing in the result of [emotion's `css` function/prop](https://emotion.sh/docs/introduction).
+	 */
+	cssOverrides?: SerializedStyles | SerializedStyles[];
 }
 
 export type TabbableElementType =
