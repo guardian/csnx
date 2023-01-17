1395512a1d3772da7b269b0667031d6175253eb9
Max Duval <max.duval@theguardian.com>
2023-01-17 16:57:09 +0000

    refactor(ab-core): export types directly

--- libs/@guardian/ab-core/src/index.ts
+++ libs/@guardian/ab-core/src/index.ts
@@ -1,17 +1,9 @@
-import type {
-	ABTest as _ABTest,
-	ABTestAPI as _ABTestAPI,
-	AbTestConfig as _AbTestConfig,
-	Participations as _Participations,
-	Runnable as _Runnable,
-	Variant as _Variant,
+export type {
+	ABTest,
+	ABTestAPI,
+	AbTestConfig,
+	Participations,
+	Runnable,
+	Variant,
 } from './@types';
-import { AB } from './ab';
-
-export { AB };
-export declare type ABTest = _ABTest;
-export declare type ABTestAPI = _ABTestAPI;
-export declare type AbTestConfig = _AbTestConfig;
-export declare type Runnable = _Runnable<_ABTest>;
-export declare type Variant = _Variant;
-export declare type Participations = _Participations;
+export { AB } from './ab';
