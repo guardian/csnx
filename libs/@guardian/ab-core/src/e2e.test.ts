1c72cf8c27fd45450feba3dae4b6a53c78175893
Joe Cowton <joe.cowton@guardian.co.uk>
2023-02-08 15:27:43 +0000

    Export CoreAPIConfig type from ab-core

---
+++ libs/@guardian/ab-core/src/e2e.test.ts
@@ -0,0 +1,22 @@
+import * as packageExports from './index';
+
+describe('The package', () => {
+	it('exports everything it did before', () => {
+		expect(Object.keys(packageExports).sort()).toEqual(['AB']);
+	});
+});
+
+// test that type exports have not been removed.
+// won't catch new types but I don't know how we can?
+export type {
+	ABTest,
+	ABTestAPI,
+	AbTestConfig,
+	CoreAPIConfig,
+	Participations,
+	Runnable,
+	Variant,
+} from './index';
+
+// @ts-expect-error: make sure the above list are real exports
+export type { ThisTypeDoesNotExist } from './index';
--- libs/@guardian/ab-core/src/index.ts
+++ libs/@guardian/ab-core/src/index.ts
@@ -2,6 +2,7 @@ export type {
 	ABTest,
 	ABTestAPI,
 	AbTestConfig,
+	CoreAPIConfig,
 	Participations,
 	Runnable,
 	Variant,
