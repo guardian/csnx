ddab456c93a3199d9174cacf488ebdc6e4722546
Max Duval <max.duval@theguardian.com>
2023-03-10 16:12:22 +0000

    wip: check and parse data

--- apps/ua-parsing/src/bq.js
+++ apps/ua-parsing/src/bq.js
@@ -1,20 +1,33 @@
 // Imports the Google Cloud client library
 import { BigQuery } from '@google-cloud/bigquery';
+import { object, array, number, string } from 'zod';
 
 const bigqueryClient = new BigQuery({
 	projectId: 'datatech-platform-prod',
 });
 
-const res = await bigqueryClient.query(`
+const date = object({
+	value: string(),
+});
+
+const row = object({
+	family: string().nullable(),
+	version: string().nullable(),
+	percent_usage: number(),
+	start_date: date,
+	end_date: date,
+});
+
+const response = await bigqueryClient.query(`
 SELECT *
-FROM public.quarterly_browser_stats
-WHERE received_date = "2023-03-07"
-ORDER BY received_timestamp DESC
-LIMIT 10
+FROM public.browser_usage_90d_window
+WHERE end_date = "2023-03-07"
+ORDER BY family ASC, version DESC
+LIMIT 1000
 `);
 
-console.log(res);
+const rows = array(row)
+	.parse(response[0])
+	.filter(({ percent_usage }) => percent_usage > 0.01 / 100);
 
-for (const r of res) {
-	console.log(r);
-}
+console.log(rows);
