1aba6ad37d556c691fd55c1f613c6512b15188ee
Alex Sanders <alex@sndrs.dev>
2023-04-26 16:57:37 +0100

    move youtube globals to global window declaration

--- @types/window.d.ts
+++ @types/window.d.ts
@@ -1,5 +1,7 @@
 import type { TeamSubscription } from '../libs/@guardian/libs/src/logger/@types/logger';
 import type { Switches } from '../libs/@guardian/libs/src/switches/@types/Switches';
+import type { google } from '../libs/@guardian/atoms-rendering/src/ima';
+import type { ImaManager } from '../libs/@guardian/atoms-rendering/src/YoutubeAtomPlayer';
 
 declare global {
 	interface Window {
@@ -37,6 +39,14 @@ declare global {
 				viewId: string;
 				pageViewId: string;
 			};
+		};
+		/**
+		 * Here we want to type the google object that will be added to window.
+		 * Since the imported google namespace is a value rather than a type, use typeof.
+		 */
+		google?: typeof google;
+		YT?: {
+			ImaManager: typeof ImaManager;
 		};
 	}
 }
