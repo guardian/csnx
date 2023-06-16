bf0ff1592819105f51ebb5ec0913f7b5979de7f3
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2023-06-16 15:39:58 +0100

    Update config to split out colour and spacing tokens

--- libs/@guardian/source-design-tokens/config.js
+++ libs/@guardian/source-design-tokens/config.js
@@ -2,11 +2,21 @@ module.exports = {
 	source: ['tokens/**/*.json'],
 	platforms: {
 		scss: {
-			transformGroup: 'css',
-			buildPath: 'build/css/',
+			transforms: ['attribute/cti', 'name/cti/kebab', 'color/css', 'size/px'],
+			buildPath: 'build/',
 			files: [
 				{
-					destination: '_variables.css',
+					filter: (token) =>
+						token.attributes.category === 'color' &&
+						token.attributes.type !== 'base', // Filter out base colours
+					destination: '_palette.css',
+					format: 'css/variables',
+				},
+				{
+					filter: (token) =>
+						token.attributes.category === 'size' &&
+						token.attributes.type === 'space',
+					destination: '_space.css',
 					format: 'css/variables',
 				},
 			],
