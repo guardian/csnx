f33d0616e07c76d4061c4c7002010c9b4b0dc8c2
Joe Cowton <77005274+joecowton1@users.noreply.github.com>
2023-06-21 11:12:34 +0100

    Update @nx/linter (#655)

--- tools/nx-plugins/eslint/executors/check/executor.ts
+++ tools/nx-plugins/eslint/executors/check/executor.ts
@@ -1,8 +1,8 @@
 import type { ExecutorContext } from '@nrwl/devkit';
-import linter from '@nrwl/linter/src/executors/eslint/lint.impl.js';
-import type { Schema } from '@nrwl/linter/src/executors/eslint/schema';
+import linter from '@nx/linter/src/executors/eslint/lint.impl.js';
+import type { Schema } from '@nx/linter/src/executors/eslint/schema';
 
-// wrap @nrwl/linter:eslint executor with some defaults
+// wrap @nx/linter:eslint executor with some defaults
 export default function (
 	options: Schema,
 	context: ExecutorContext,
