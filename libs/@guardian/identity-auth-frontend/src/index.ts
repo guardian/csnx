d61dad1b0024cb246e953d0844762b1be2812aba
Ashleigh Carr <ashcorr20@gmail.com>
2023-08-14 15:59:36 +0100

    Use `guard` for Stage & simplify unintialized window behaviour.
    Co-authored-by: Max Duval <max.duval@theguardian.com>

--- libs/@guardian/identity-auth-frontend/src/index.ts
+++ libs/@guardian/identity-auth-frontend/src/index.ts
@@ -1,5 +1,10 @@
 import { IdentityAuth } from '@guardian/identity-auth';
 import type { CustomClaims } from '@guardian/identity-auth';
+import { guard } from '@guardian/identity-auth/cjs/@types/guard';
+
+const stages = ['PROD', 'CODE', 'DEV'] as const;
+type Stage = (typeof stages)[number];
+const isStage = guard(stages);
 
 type FrontendIdTokenClaims = CustomClaims & {
 	email: string;
@@ -8,17 +13,18 @@ type FrontendIdTokenClaims = CustomClaims & {
 
 export type FrontendIdentityAuth = IdentityAuth<never, FrontendIdTokenClaims>;
 
-const getStage = (isDev: boolean, stage: string) => (isDev ? 'DEV' : stage);
+const getStage = (isDev: boolean, stage: string) =>
+	isDev || !isStage(stage) ? 'DEV' : stage;
 
-const getIssuer = (stage: string) =>
+const getIssuer = (stage: Stage) =>
 	stage === 'PROD'
 		? 'https://profile.theguardian.com/oauth2/aus3xgj525jYQRowl417'
 		: 'https://profile.code.dev-theguardian.com/oauth2/aus3v9gla95Toj0EE0x7';
 
-const getClientId = (stage: string) =>
+const getClientId = (stage: Stage) =>
 	stage === 'PROD' ? '0oa79m1fmgzrtaHc1417' : '0oa53x6k5wGYXOGzm0x7';
 
-const getRedirectUri = (stage: string) => {
+const getRedirectUri = (stage: Stage) => {
 	switch (stage) {
 		case 'PROD':
 			return 'https://www.theguardian.com/';
@@ -31,36 +37,37 @@ const getRedirectUri = (stage: string) => {
 };
 
 export const getIdentityAuth = () => {
-	const { guardian } = window;
-	const { config } = guardian ?? {};
-	const { isDev, stage } = guardian?.config ?? {};
-
-	if (!guardian || !config || !isDev || !stage) {
+	if (!window.guardian)
 		throw new Error('window.guardian has not yet been initialized');
-	}
 
+	const {
+		isDev = false,
+		stage = 'PROD',
+		switches,
+	} = window.guardian.config ?? {};
 	const stageOrDev = getStage(isDev, stage);
 
-	if (!guardian.identityAuth) {
-		guardian.identityAuth = new IdentityAuth<never, FrontendIdTokenClaims>({
-			issuer: getIssuer(stageOrDev),
-			clientId: getClientId(stageOrDev),
-			redirectUri: getRedirectUri(stageOrDev),
-			idCookieSessionRefresh: config.switches?.idCookieRefresh ?? false,
-			scopes: [
-				'openid', // required for open id connect, returns an id token
-				'profile', // populates the id token with basic profile information
-				'email', // populates the id token with the user's email address
-				'guardian.discussion-api.private-profile.read.self', // allows the access token to be used to make requests to the discussion api to read the user's profile
-				'guardian.discussion-api.update.secure', // allows the access token to be used to make requests to the discussion api to post comments, upvote etc
-				'guardian.identity-api.newsletters.read.self', // allows the access token to be used to make requests to the identity api to read the user's newsletter subscriptions
-				'guardian.identity-api.newsletters.update.self', // allows the access token to be used to make requests to the identity api to update the user's newsletter subscriptions
-				'guardian.identity-api.user.username.create.self.secure', // allows the access token to set the user's username
-				'guardian.members-data-api.read.self', // allows the access token to be used to make requests to the members data api to read the user's membership status
-				'id_token.profile.theguardian', // populates the id token with application specific profile information
-			],
-		});
-	}
+	window.guardian.identityAuth ||= new IdentityAuth<
+		never,
+		FrontendIdTokenClaims
+	>({
+		issuer: getIssuer(stageOrDev),
+		clientId: getClientId(stageOrDev),
+		redirectUri: getRedirectUri(stageOrDev),
+		idCookieSessionRefresh: switches?.idCookieRefresh ?? false,
+		scopes: [
+			'openid', // required for open id connect, returns an id token
+			'profile', // populates the id token with basic profile information
+			'email', // populates the id token with the user's email address
+			'guardian.discussion-api.private-profile.read.self', // allows the access token to be used to make requests to the discussion api to read the user's profile
+			'guardian.discussion-api.update.secure', // allows the access token to be used to make requests to the discussion api to post comments, upvote etc
+			'guardian.identity-api.newsletters.read.self', // allows the access token to be used to make requests to the identity api to read the user's newsletter subscriptions
+			'guardian.identity-api.newsletters.update.self', // allows the access token to be used to make requests to the identity api to update the user's newsletter subscriptions
+			'guardian.identity-api.user.username.create.self.secure', // allows the access token to set the user's username
+			'guardian.members-data-api.read.self', // allows the access token to be used to make requests to the members data api to read the user's membership status
+			'id_token.profile.theguardian', // populates the id token with application specific profile information
+		],
+	});
 
-	return guardian.identityAuth;
+	return window.guardian.identityAuth;
 };
