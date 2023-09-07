191f0e6045dc7a822f309964815ff36be0f8d765
David Blatcher <dv.bltchr@gmail.com>
2023-09-07 11:53:00 +0100

    add folder for newsletters types

---
+++ libs/@guardian/libs/src/newsletters/@types/index.ts
@@ -0,0 +1,3 @@
+export * from './newsletters-api';
+
+// TO DO - re-export everything from libs/@guardian/libs/src/index.ts
---
+++ libs/@guardian/libs/src/newsletters/@types/newsletters-api.ts
@@ -0,0 +1,104 @@
+export type AutomatedFrontSection = {
+	subheading: string;
+	insertPosition: number;
+	frontPath: string;
+	sectionName: string;
+	maxArticles: number;
+	partBuilderFunction: string;
+};
+
+export type NewsletterEmailRenderingOptions = {
+	linkListSubheading?: string[] | undefined;
+	podcastSubheading?: string[] | undefined;
+	darkThemeSubheading?: string[] | undefined;
+	readMoreSections?:
+		| {
+				subheading: string;
+				wording: string;
+				onwardPath?: string;
+				isDarkTheme?: boolean;
+		  }[]
+		| undefined;
+
+	automatedFrontSections?: AutomatedFrontSection[] | undefined;
+	displayDate: boolean;
+	displayStandfirst: boolean;
+	contactEmail: string;
+	displayImageCaptions: boolean;
+	mainBannerUrl?: string;
+	subheadingBannerUrl?: string;
+	darkSubheadingBannerUrl?: string;
+	darkHeadlineBackground?: boolean;
+	displayNewsletterName?: boolean;
+	paletteOverride?:
+		| 'news'
+		| 'opinion'
+		| 'culture'
+		| 'sport'
+		| 'lifestyle'
+		| 'features';
+};
+
+// have excluded some of the fields from the api data which are
+// used purely for data collection and workflow management
+// internally in the newsletters tool.
+// Other application should not rely on these.
+export type NewsletterApiData = {
+	identityName: string;
+	name: string;
+	category:
+		| 'article-based'
+		| 'article-based-legacy'
+		| 'fronts-based'
+		| 'manual-send'
+		| 'other';
+	restricted: boolean;
+	status: 'paused' | 'cancelled' | 'live' | 'pending';
+	emailConfirmation: boolean;
+	brazeSubscribeAttributeName: string;
+	brazeSubscribeEventNamePrefix: string;
+	brazeNewsletterName: string;
+	theme: 'news' | 'opinion' | 'culture' | 'sport' | 'lifestyle' | 'features';
+	group: string;
+	signUpHeadline: string;
+	signUpDescription: string;
+	signUpEmbedDescription: string;
+	regionFocus?: 'UK' | 'AU' | 'US' | 'INTL' | 'EUR';
+	frequency: string;
+	listId: number;
+	listIdV1: number;
+	campaignName?: string;
+	campaignCode?: string;
+	brazeSubscribeAttributeNameAlternate?: string[];
+	signupPage?: string;
+	exampleUrl?: string;
+	illustrationCircle?: string;
+	illustrationCard?: string;
+	creationTimeStamp: number;
+	cancellationTimeStamp?: number;
+	seriesTag?: string;
+	composerCampaignTag?: string;
+
+	renderingOptions?: NewsletterEmailRenderingOptions;
+	mailSuccessDescription?: string;
+};
+
+type ApiErrorResponse = {
+	ok: false;
+	message?: string;
+};
+
+type ApiSuccessResponse<T> = {
+	ok: true;
+	total: number;
+	data: T;
+};
+
+type ApiResponse<T = unknown> = ApiSuccessResponse<T> | ApiErrorResponse;
+
+export type NewslettersApiAllNewslettersResponse = ApiResponse<
+	NewsletterApiData[]
+>;
+
+export type NewslettersApiSingleNewslettersResponse =
+	ApiResponse<NewsletterApiData>;
