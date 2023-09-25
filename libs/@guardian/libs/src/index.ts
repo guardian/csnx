a1da5b1cc0912c9c6f2b2d12d384089b771b0076
Max Duval <max.duval@theguardian.com>
2023-09-25 09:14:20 +0100

    refactor: order in barrel file
    also add to top-level docs
    
    Co-authored-by: Alex Sanders <alex@sndrs.dev>

--- libs/@guardian/libs/src/index.ts
+++ libs/@guardian/libs/src/index.ts
@@ -25,8 +25,8 @@ export { Pillar } from './format/Pillar';
 
 export { isBoolean } from './isBoolean/isBoolean';
 export { isNonNullable } from './isNonNullable/isNonNullable';
-export { isOneOf } from './isOneOf/isOneOf';
 export { isObject } from './isObject/isObject';
+export { isOneOf } from './isOneOf/isOneOf';
 export { isString } from './isString/isString';
 export { isUndefined } from './isUndefined/isUndefined';
 
