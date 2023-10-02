5eb003bd9140a0214007aefbd2b677ca33f0d7a1
David Blatcher <dv.bltchr@gmail.com>
2023-10-02 17:32:43 +0100

    move types from libs to new package

--- libs/@csnx/newsletter-types/src/index.ts
+++ libs/@csnx/newsletter-types/src/index.ts
@@ -1,6 +1,6 @@
+export * from './@types/';
+
 export type TestType = 'red' | 'blue';
-
 export const valids: TestType[] = ['red', 'blue'];
-
 export const testValidate = (input: unknown): input is TestType =>
 	typeof input === 'string' && (valids as string[]).includes(input);
