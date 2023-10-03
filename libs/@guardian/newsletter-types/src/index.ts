f9ce18ab60327f13809a3ecfd108f1eb1fb7f04a
David Blatcher <dv.bltchr@gmail.com>
2023-10-03 10:18:42 +0100

    remove test test

--- libs/@guardian/newsletter-types/src/index.ts
+++ libs/@guardian/newsletter-types/src/index.ts
@@ -1,6 +1,1 @@
 export * from './@types/';
-
-export type TestType = 'red' | 'blue';
-export const valids: TestType[] = ['red', 'blue'];
-export const testValidate = (input: unknown): input is TestType =>
-	typeof input === 'string' && (valids as string[]).includes(input);
