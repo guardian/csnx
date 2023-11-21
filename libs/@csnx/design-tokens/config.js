867fca53c7f25e723c26aca2acadecea199da041
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2023-11-21 15:36:24 +0000

    Clean up custom format

--- libs/@csnx/design-tokens/config.js
+++ libs/@csnx/design-tokens/config.js
@@ -2,30 +2,21 @@ const StyleDictionary = require('style-dictionary');
 const _ = require('lodash');
 
 StyleDictionary.registerFormat({
-	name: 'es6Object',
+	name: 'typeScriptObject',
 	formatter: function ({ dictionary }) {
 		const tokens = dictionary.allTokens
 			.map((token) => {
-				let value = JSON.stringify(token.value);
-				// the `dictionary` object now has `usesReference()` and
-				// `getReferences()` methods. `usesReference()` will return true if
-				// the value has a reference in it. `getReferences()` will return
-				// an array of references to the whole tokens so that you can access their
-				// names or any other attributes.
+				const value = JSON.stringify(token.value);
 				if (dictionary.usesReference(token.original.value)) {
-					// Note: make sure to use `token.original.value` because
-					// `token.value` is already resolved at this point.
 					const refs = dictionary.getReferences(token.original.value);
 					refs.forEach((ref) => {
-						value = value.replace(ref.value, function () {
-							return `${ref.name}`;
-						});
+						value = value.replace(ref.value, () => `${ref.name}`);
 					});
 				}
 				return `${token.name}: ${value},`;
 			})
 			.join('\n');
-
+		// TODO: get object variable name from token
 		return `export const space = { ${tokens} } as const;`;
 	},
 });
@@ -33,7 +24,6 @@ StyleDictionary.registerFormat({
 StyleDictionary.registerTransform({
 	name: 'name/item/camel',
 	type: 'name',
-	// transformer: (token, options) => _.camelCase( [options.prefix].concat(token.path.slice(token.path.length - 1).join(' ')) ),
 	transformer: (token) => token.path.slice(token.path.length - 1),
 });
 
@@ -46,7 +36,7 @@ module.exports = {
 			files: [
 				{
 					filter: (token) => token.attributes.category === 'size',
-					format: 'es6Object',
+					format: 'typeScriptObject',
 					destination: 'space.ts',
 				},
 			],
