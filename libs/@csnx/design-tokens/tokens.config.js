bd80dc91ab2ef38fde5d838bec480f367f4805aa
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2023-11-28 17:07:52 +0000

    Add size and breakpoint tokens

--- libs/@csnx/design-tokens/tokens.config.js
+++ libs/@csnx/design-tokens/tokens.config.js
@@ -2,8 +2,10 @@ import pluginCSS, { defaultNameGenerator } from '@cobalt-ui/plugin-css';
 
 export default {
 	tokens: [
+		'./src/breakpoint.tokens.json',
 		'./src/colour.tokens.json',
 		'./src/palette.tokens.json',
+		'./src/size.tokens.json',
 		'./src/space.tokens.json',
 	],
 	outDir: './dist/',
