581e7f50603814efc09b6209ab547281a0c33fca
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2023-12-04 15:40:23 +0000

    Configure Cobalt to output tokens as nested JS object

--- libs/@csnx/design-tokens/tokens.config.js
+++ libs/@csnx/design-tokens/tokens.config.js
@@ -1,12 +1,25 @@
 import pluginCSS, { defaultNameGenerator } from '@cobalt-ui/plugin-css';
+import pluginJS from '@cobalt-ui/plugin-js';
 
-const prefixName = (variableId) => {
+const prefixCustomProperty = (variableId) => {
 	return defaultNameGenerator(`source.${variableId}`);
 };
 
 const pxToRem = (token) => {
 	if (token.$type === 'dimension' && token.$value.slice(-2) === 'px') {
 		return token.$value.slice(0, -2) / 16 + 'rem';
+	}
+};
+
+const pxToNumeric = (token) => {
+	const tokenGroup = token.id.split('.')[0];
+
+	if (
+		token.$type === 'dimension' &&
+		token.$value.slice(-2) === 'px' &&
+		tokenGroup === 'space'
+	) {
+		return Number(token.$value.slice(0, -2));
 	}
 };
 
@@ -23,8 +36,14 @@ export default {
 	plugins: [
 		pluginCSS({
 			p3: false,
-			generateName: prefixName,
+			generateName: prefixCustomProperty,
 			transform: pxToRem,
+		}),
+		pluginJS({
+			js: true,
+			meta: false,
+			deep: true,
+			transform: pxToNumeric,
 		}),
 	],
 };
