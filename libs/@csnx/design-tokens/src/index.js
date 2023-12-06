b2b6b13f459f27523f308491c44abdae14d1a651
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2023-12-06 12:19:58 +0000

    Add palette export and types

--- libs/@csnx/design-tokens/src/index.js
+++ libs/@csnx/design-tokens/src/index.js
@@ -1,15 +1,18 @@
 import { tokens } from './build';
 
+const palette = tokens.palette;
 const space = {};
 const remSpace = {};
 
-// Convert px spacing tokens to unitless numeric values and rems
-// to match existing format in Source Foundations
+/**
+ * Convert `px` spacing tokens to unitless numeric values and `rem` equivalent
+ * to match existing Source Foundation spacing token structure
+ */
 Object.keys(tokens.space).forEach((key) => {
-	// Strip 'px' unit from token and convert to a numeric value
+	// Strip `px` unit from token and convert to numeric value
 	const value = Number(tokens.space[key].slice(0, -2));
 	space[key] = value;
 	remSpace[key] = `${value / 16}rem`;
 });
 
-export { tokens, space, remSpace };
+export { palette, space, remSpace };
