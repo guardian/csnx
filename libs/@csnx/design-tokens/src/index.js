a25d986bc75b843d556ef372c3d41342f4ffb811
oliverabrahams <ollie.abrahams@guardian.co.uk>
2023-12-06 16:55:55 +0000

    a view as to how we want the new output to be for the ts file

--- libs/@csnx/design-tokens/src/index.js
+++
@@ -1,9 +0,0 @@
-import { tokens } from './build';
-import { addRem, removePx } from './utils/value-conversions.js';
-
-const palette = tokens.palette;
-const breakpoints = removePx(tokens.breakpoint);
-const space = removePx(tokens.space);
-const remSpace = addRem(space);
-
-export { palette, space, remSpace, breakpoints };
---
+++ libs/@csnx/design-tokens/src/index.ts
@@ -0,0 +1,8 @@
+import { tokens } from './build';
+
+const palette = tokens.palette;
+const breakpoints = tokens.breakpoint;
+const space = tokens.space;
+const remSpace = tokens.remSpace;
+
+export { palette, space, remSpace, breakpoints };
