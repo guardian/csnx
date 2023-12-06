229237b3f491fc97863458eb123d638d218529d9
oliverabrahams <ollie.abrahams@guardian.co.uk>
2023-12-06 15:15:21 +0000

    added readonly to the objects

--- libs/@csnx/design-tokens/src/index.d.ts
+++ libs/@csnx/design-tokens/src/index.d.ts
@@ -1,9 +1,13 @@
 import { tokens } from './build';
 
-export declare const breakpoint: {
+export declare const breakpoints: Readonly<{
 	[K in keyof typeof tokens.breakpoint]: number;
-};
-export declare const palette: typeof tokens.palette;
+}>;
+export declare const palette: Readonly<typeof tokens.palette>;
 
-export declare const space: { [K in keyof typeof tokens.space]: number };
-export declare const remSpace: { [K in keyof typeof tokens.space]: string };
+export declare const space: Readonly<{
+	[K in keyof typeof tokens.space]: number;
+}>;
+export declare const remSpace: Readonly<{
+	[K in keyof typeof tokens.space]: string;
+}>;
--- libs/@csnx/design-tokens/src/index.js
+++ libs/@csnx/design-tokens/src/index.js
@@ -2,8 +2,8 @@ import { tokens } from './build';
 import { addRem, removePx } from './utils/value-conversions.js';
 
 const palette = tokens.palette;
-const breakpoint = tokens.breakpoint;
+const breakpoints = removePx(tokens.breakpoint);
 const space = removePx(tokens.space);
 const remSpace = addRem(space);
 
-export { palette, space, remSpace, breakpoint };
+export { palette, space, remSpace, breakpoints };
