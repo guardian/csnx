4c153deebf1b17d4dd1e0fba8d774d83c6956da3
oliverabrahams <ollie.abrahams@guardian.co.uk>
2023-12-08 09:26:52 +0000

    added breakpoints and palette

--- libs/@guardian/source-foundations/src/breakpoints/breakpoints.ts
+++ libs/@guardian/source-foundations/src/breakpoints/breakpoints.ts
@@ -1,24 +1,18 @@
-export type Breakpoint =
-	| 'mobile'
-	| 'mobileMedium'
-	| 'mobileLandscape'
-	| 'phablet'
-	| 'tablet'
-	| 'desktop'
-	| 'leftCol'
-	| 'wide';
+import { tokens } from '@csnx/design-tokens';
 
+type Breakpoint = keyof typeof tokens.breakpoint;
+
+const breakpoints = {} as { [K in Breakpoint]: number };
 /**
  * [Storybook](https://guardian.github.io/csnx/?path=/docs/source-foundations_media-queries--page) •
  * [Design System](https://theguardian.design/2a1e5182b/p/41be19-grids)
  */
-export const breakpoints = {
-	mobile: 320,
-	mobileMedium: 375,
-	mobileLandscape: 480,
-	phablet: 660,
-	tablet: 740,
-	desktop: 980,
-	leftCol: 1140,
-	wide: 1300,
-} as const;
+Object.keys(tokens.breakpoint).forEach((key) => {
+	// Strip 'px' unit from token and convert to a numeric value
+	breakpoints[key as Breakpoint] = Number(
+		tokens.breakpoint[key as Breakpoint].slice(0, -2),
+	);
+});
+
+export { breakpoints };
+export type { Breakpoint };
