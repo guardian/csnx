80bff6ad4c0e21cac140493684b2099f1fa8a9b9
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-01-04 12:00:28 +0000

    Add snapshot tests for token output

---
+++ libs/@guardian/source-foundations/src/tokens.test.ts
@@ -0,0 +1,123 @@
+import { transitions } from './animation/transitions';
+import { breakpoints } from './breakpoints/breakpoints';
+import {
+	background,
+	border,
+	brandAltBackground,
+	brandAltBorder,
+	brandAltLine,
+	brandAltText,
+	brandBackground,
+	brandBorder,
+	brandLine,
+	brandText,
+	line,
+	palette,
+	text,
+} from './colour/palette';
+import {
+	height,
+	iconSize,
+	remHeight,
+	remWidth,
+	size,
+	width,
+} from './size/size';
+import { remSpace, space } from './space/space';
+import {
+	availableFontWeights,
+	bodySizes,
+	fonts,
+	fontWeights,
+	headlineSizes,
+	italicsFontWeights,
+	lineHeights,
+	pxTextSizes,
+	remBodySizes,
+	remHeadlineSizes,
+	remTextSansSizes,
+	remTextSizes,
+	remTitlepieceSizes,
+	textSansSizes,
+	titlepieceSizes,
+	underlineThickness,
+} from './typography/data';
+
+/**
+ * Verify token output with snapshot tests.
+ *
+ * This is an additional safety net to catch instances of token values that have
+ * been accidentally changed, or tokens that have been removed or missed.
+ *
+ * If these tests fail, please *do not* automatically update the snapshots
+ * without checking that the output matches what you are expecting to see.
+ */
+
+describe('transitions', () => {
+	it('matches existing token values in snapshot', () => {
+		expect(transitions).toMatchSnapshot();
+	});
+});
+
+describe('breakpoints', () => {
+	it('matches existing token values in snapshot', () => {
+		expect(breakpoints).toMatchSnapshot();
+	});
+});
+
+describe('palette', () => {
+	it('matches existing token values in snapshot', () => {
+		expect(palette).toMatchSnapshot();
+		expect(background).toMatchSnapshot();
+		expect(brandBackground).toMatchSnapshot();
+		expect(brandAltBackground).toMatchSnapshot();
+		expect(border).toMatchSnapshot();
+		expect(brandBorder).toMatchSnapshot();
+		expect(brandAltBorder).toMatchSnapshot();
+		expect(line).toMatchSnapshot();
+		expect(brandLine).toMatchSnapshot();
+		expect(brandAltLine).toMatchSnapshot();
+		expect(text).toMatchSnapshot();
+		expect(brandText).toMatchSnapshot();
+		expect(brandAltText).toMatchSnapshot();
+	});
+});
+
+describe('size', () => {
+	it('matches existing token values in snapshot', () => {
+		expect(size).toMatchSnapshot();
+		expect(iconSize).toMatchSnapshot();
+		expect(height).toMatchSnapshot();
+		expect(remHeight).toMatchSnapshot();
+		expect(width).toMatchSnapshot();
+		expect(remWidth).toMatchSnapshot();
+	});
+});
+
+describe('space', () => {
+	it('matches existing token values in snapshot', () => {
+		expect(space).toMatchSnapshot();
+		expect(remSpace).toMatchSnapshot();
+	});
+});
+
+describe('typography', () => {
+	it('matches existing token values in snapshot', () => {
+		expect(pxTextSizes).toMatchSnapshot();
+		expect(remTextSizes).toMatchSnapshot();
+		expect(fonts).toMatchSnapshot();
+		expect(lineHeights).toMatchSnapshot();
+		expect(fontWeights).toMatchSnapshot();
+		expect(availableFontWeights).toMatchSnapshot();
+		expect(italicsFontWeights).toMatchSnapshot();
+		expect(underlineThickness).toMatchSnapshot();
+		expect(textSansSizes).toMatchSnapshot();
+		expect(bodySizes).toMatchSnapshot();
+		expect(headlineSizes).toMatchSnapshot();
+		expect(titlepieceSizes).toMatchSnapshot();
+		expect(remTextSansSizes).toMatchSnapshot();
+		expect(remBodySizes).toMatchSnapshot();
+		expect(remHeadlineSizes).toMatchSnapshot();
+		expect(remTitlepieceSizes).toMatchSnapshot();
+	});
+});
