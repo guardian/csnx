7cd9bf1c9a5b721d2ddf22d88ffd82f0d4ca7680
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-01-12 16:29:27 +0000

    Get Choice Card theme from design tokens

--- libs/@guardian/source-react-components/src/choice-card/theme.ts
+++ libs/@guardian/source-react-components/src/choice-card/theme.ts
@@ -1,3 +1,4 @@
+import { tokens } from '@csnx/design-tokens';
 import { palette } from '@guardian/source-foundations';
 import { userFeedbackThemeDefault } from '../user-feedback/theme';
 
@@ -19,6 +20,7 @@ export interface ChoiceCardTheme {
 export type ChoiceCardFullTheme = {
 	[P in keyof ChoiceCardTheme]-?: ChoiceCardTheme[P];
 };
+
 /** @deprecated Use `choiceCardThemeLight` and `theme` prop instead of `ThemeProvider` **/
 export const choiceCardThemeDefault = {
 	choiceCard: {
@@ -39,17 +41,19 @@ export const choiceCardThemeDefault = {
 	...userFeedbackThemeDefault,
 };
 
+const { choiceCard } = tokens;
+
 export const choiceCardTheme: ChoiceCardFullTheme = {
-	textUnselected: palette.neutral[46],
-	textSelected: palette.brand[400],
-	textHover: palette.brand[500],
-	textError: palette.error[400],
-	borderUnselected: palette.neutral[46],
-	borderSelected: palette.brand[500],
-	borderHover: palette.brand[500],
-	borderError: palette.error[400],
-	backgroundUnselected: 'transparent',
-	backgroundHover: 'transparent',
-	backgroundSelected: '#E3F6FF',
-	backgroundTick: palette.brand[500],
+	textUnselected: choiceCard.color.text.unselected,
+	textSelected: choiceCard.color.text.selected,
+	textHover: choiceCard.color.text.hover,
+	textError: choiceCard.color.text.error,
+	borderUnselected: choiceCard.color.border.unselected,
+	borderSelected: choiceCard.color.border.selected,
+	borderHover: choiceCard.color.border.hover,
+	borderError: choiceCard.color.border.error,
+	backgroundUnselected: choiceCard.color.background.unselected,
+	backgroundHover: choiceCard.color.background.hover,
+	backgroundSelected: choiceCard.color.background.selected,
+	backgroundTick: choiceCard.color.background.tick,
 };
