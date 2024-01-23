248c9dccdf569e1aa3175af5b89ca1644dc2879f
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-01-23 15:32:12 +0000

    Update .eslintrc.js
    Co-authored-by: Alex Sanders <alex@sndrs.dev>

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
 					{
 						enforceBuildableLibDependency: true,
 						// These packages may not need to build because they are not published.
-						allow: ['@guardian/design-tokens'],
+						allow: ['@csnx/**', '@guardian/design-tokens'],
 						depConstraints: [
 							{
 								sourceTag: '*',
