22b388ef7627be519ec9b0dbca51f4baa9215131
Alex Sanders <alex@sndrs.dev>
2024-01-24 15:48:54 +0000

    readonly is enough
    Co-authored-by: Max Duval <max.duval@theguardian.com>

--- libs/@guardian/source-foundations/src/utils/convert-value.ts
+++ libs/@guardian/source-foundations/src/utils/convert-value.ts
@@ -2,6 +2,6 @@ export const rootPixelFontSize = 16;
 export const pxToRem = (px: number): number => px / rootPixelFontSize;
 export const pxStringToNumber = (px: string): number => Number(px.slice(0, -2));
 export const fontArrayToString = (
-	fonts: string[] | readonly string[],
+	fonts: readonly string[],
 ): string =>
 	fonts.map((name) => (name.includes(' ') ? `"${name}"` : name)).join(', ');
