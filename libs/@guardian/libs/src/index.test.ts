41179c06e8a8bce2ea68c33c0c845fd863cfe3c0
Anna Voelker <anna.voelker@guardian.co.uk>
2024-02-01 14:11:29 +0000

    index.ts

--- libs/@guardian/libs/src/index.test.ts
+++ libs/@guardian/libs/src/index.test.ts
@@ -1,4 +1,9 @@
+import waitForExpect from 'wait-for-expect';
+import { CMP as actualCMP } from './consent-management-platform/cmp';
+import { disable, enable } from './consent-management-platform/disable';
+import { getCurrentFramework } from './consent-management-platform/getCurrentFramework';
 import * as packageExports from './index';
+import { cmp } from './index';
 
 describe('The package', () => {
 	it('exports everything it did before', () => {
@@ -55,3 +60,190 @@ export type {
 
 // @ts-expect-error: make sure the above list are real exports
 export type { ThisTypeDoesNotExist } from './index';
+
+// *************** TESTS for consent-management-platform ***************
+
+const CMP = {
+	init: jest.spyOn(actualCMP, 'init'),
+	showPrivacyManager: jest.spyOn(actualCMP, 'showPrivacyManager'),
+};
+
+beforeEach(() => {
+	window._sp_ = undefined;
+	window.guCmpHotFix.initialised = false;
+	CMP.init.mockClear();
+});
+
+describe('cmp.init', () => {
+	it('does nothing if CMP is disabled', () => {
+		disable();
+
+		cmp.init({ country: 'GB' });
+		cmp.init({ country: 'US' });
+
+		expect(CMP.init).not.toHaveBeenCalled();
+
+		enable();
+	});
+
+	it('requires country to be set', () => {
+		expect(() => {
+			cmp.init({ pubData: {} });
+		}).toThrow('required');
+	});
+
+	it('initializes CMP when in the US', () => {
+		cmp.init({ country: 'US' });
+		expect(CMP.init).toHaveBeenCalledTimes(1);
+	});
+
+	it('initializes CMP when in Australia', () => {
+		cmp.init({ country: 'AU' });
+		expect(CMP.init).toHaveBeenCalledTimes(1);
+	});
+
+	it('initializes TCF when neither in the US or Australia', () => {
+		cmp.init({ country: 'GB' });
+		expect(CMP.init).toHaveBeenCalledTimes(1);
+	});
+});
+
+// *************** START commercial.dcr.js hotfix ***************
+describe('hotfix cmp.init', () => {
+	it('only initialises once per page', () => {
+		cmp.init({ country: 'GB' });
+		cmp.init({ country: 'GB' });
+		cmp.init({ country: 'GB' });
+		cmp.init({ country: 'GB' });
+		expect(CMP.init).toHaveBeenCalledTimes(1);
+		expect(window.guCmpHotFix.initialised).toBe(true);
+	});
+
+	it('warn if two versions are running simultaneously', () => {
+		global.console.warn = jest.fn();
+		cmp.init({ country: 'GB' });
+		const currentVersion = window.guCmpHotFix.cmp.version;
+		const mockedVersion = 'X.X.X-mock';
+		global.guCmpHotFix.cmp.version = mockedVersion;
+
+		cmp.init({ country: 'GB' });
+
+		expect(global.console.warn).toHaveBeenCalledWith(
+			'Two different versions of the CMP are running:',
+			[currentVersion, mockedVersion],
+		);
+	});
+
+	it.each([
+		['GB', 'tcfv2'],
+		['AU', 'aus'],
+		['US', 'ccpa'],
+		['YT', 'tcfv2'],
+		['FR', 'tcfv2'],
+		['CA', 'tcfv2'],
+		['NZ', 'tcfv2'],
+	])('In %s, use the %s framework correctly', (country, framework) => {
+		cmp.init({ country });
+		expect(getCurrentFramework()).toEqual(framework);
+	});
+
+	it('uses window.guCmpHotFix instances if they exist', () => {
+		const mockCmp = {
+			init: () => undefined,
+			willShowPrivacyMessage: () => true,
+			willShowPrivacyMessageSync: () => true,
+			hasInitialised: () => true,
+			mocked: 'mocked',
+		};
+
+		window.guCmpHotFix = {
+			cmp: mockCmp,
+		};
+
+		jest.resetModules();
+		import('./index').then((module) => {
+			expect(module.cmp).toEqual(mockCmp);
+
+			delete window.guCmpHotFix;
+			jest.resetModules();
+			import('./index');
+		});
+	});
+});
+// *************** END commercial.dcr.js hotfix ***************
+
+describe('cmp.willShowPrivacyMessage', () => {
+	it('resolves regardless of when the cmp is initialised', () => {
+		// This should be tested in e2e test to be meaningful
+		const willShowPrivacyMessage1 = cmp.willShowPrivacyMessage();
+
+		cmp.init({ country: 'US' });
+
+		const willShowPrivacyMessage2 = cmp.willShowPrivacyMessage();
+
+		cmp.willShowPrivacyMessage().then(() => {
+			expect(
+				Promise.all([willShowPrivacyMessage1, willShowPrivacyMessage2]),
+			).resolves.toEqual([true, true]);
+		});
+	});
+});
+
+describe('cmp.willShowPrivacyMessageSync', () => {
+	it('throws if CMP is not initialised', () => {
+		expect(() => cmp.willShowPrivacyMessageSync()).toThrow();
+	});
+
+	it('does not throw if CMP is initialised', () => {
+		cmp.init({ country: 'GB' });
+
+		cmp.willShowPrivacyMessage().then(() => {
+			expect(() => cmp.willShowPrivacyMessageSync()).not.toThrow();
+		});
+	});
+});
+
+describe('cmp.hasInitialised', () => {
+	it('returns false if CMP is not initialised', () => {
+		expect(cmp.hasInitialised()).toBe(false);
+	});
+
+	it('returns true when CMP is initialised', () => {
+		cmp.init({ country: 'GB' });
+
+		cmp.willShowPrivacyMessage().then(() => {
+			expect(cmp.hasInitialised()).toBe(true);
+		});
+	});
+});
+
+describe('cmp.showPrivacyManager', () => {
+	it('shows CMP privacy manager when in the US', () => {
+		cmp.init({ country: 'US' });
+
+		cmp.showPrivacyManager();
+
+		return waitForExpect(() =>
+			expect(CMP.showPrivacyManager).toHaveBeenCalledTimes(1),
+		);
+	});
+
+	it('shows CMP privacy manager when in Australia', () => {
+		cmp.init({ country: 'AU' });
+
+		cmp.showPrivacyManager();
+
+		return waitForExpect(() =>
+			expect(CMP.showPrivacyManager).toHaveBeenCalledTimes(1),
+		);
+	});
+	it('shows TCF privacy manager when neither in the US or Australia', () => {
+		cmp.init({ country: 'GB' });
+
+		cmp.showPrivacyManager();
+
+		return waitForExpect(() =>
+			expect(CMP.showPrivacyManager).toHaveBeenCalledTimes(1),
+		);
+	});
+});
