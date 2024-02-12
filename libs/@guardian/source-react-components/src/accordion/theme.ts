22f08228cd3f90ec77886e798f54acfd87514866
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-02-12 11:08:00 +0000

    Update theme properties and add icon fill

--- libs/@guardian/source-react-components/src/accordion/theme.ts
+++ libs/@guardian/source-react-components/src/accordion/theme.ts
@@ -1,27 +1,24 @@
 import { palette } from '@guardian/source-foundations';
 import type { Theme } from '../@types/Theme';
 
-export const transformProviderTheme = (
-	providerTheme: Theme['accordion'],
-): Partial<ThemeAccordion> => {
-	const transformedTheme: Partial<ThemeAccordion> = {};
-
-	if (providerTheme?.textPrimary) {
-		transformedTheme.label = providerTheme.textPrimary;
-	}
-	if (providerTheme?.borderPrimary) {
-		transformedTheme.border = providerTheme.borderPrimary;
-	}
-
-	return transformedTheme;
-};
-
 export type ThemeAccordion = {
-	text: string;
+	textLabel: string;
+	textBody: string;
+	textCta: string;
 	border: string;
-	label: string;
-	ctaText: string;
+	iconFill: string;
 };
+
+export const themeAccordion: ThemeAccordion = {
+	textLabel: palette.neutral[7],
+	textBody: palette.neutral[7],
+	textCta: palette.neutral[7],
+	border: palette.neutral[60],
+	iconFill: palette.neutral[7],
+} as const;
+
+/** @deprecated Use `themeAccordion` and component `theme` prop instead of emotion's `ThemeProvider` */
+
 export const accordionThemeDefault = {
 	accordion: {
 		textPrimary: palette.neutral[7],
@@ -29,9 +26,20 @@ export const accordionThemeDefault = {
 	},
 };
 
-export const themeAccordion: ThemeAccordion = {
-	label: palette.neutral[7],
-	text: palette.neutral[7],
-	border: palette.neutral[60],
-	ctaText: palette.neutral[7],
+export const transformProviderTheme = (
+	providerTheme: Theme['accordion'],
+): Partial<ThemeAccordion> => {
+	const transformedTheme: Partial<ThemeAccordion> = {};
+
+	if (providerTheme?.textPrimary) {
+		transformedTheme.textLabel = providerTheme.textPrimary;
+		transformedTheme.textBody = providerTheme.textPrimary;
+		transformedTheme.textCta = providerTheme.textPrimary;
+		transformedTheme.iconFill = providerTheme.textPrimary;
+	}
+	if (providerTheme?.borderPrimary) {
+		transformedTheme.border = providerTheme.borderPrimary;
+	}
+
+	return transformedTheme;
 };
