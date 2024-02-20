118c857b33e2be44366b6dc97159f1e962abfe33
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-02-20 11:06:17 +0000

    Updated the composed Storybook to point at Chromatic builds instead of Amplify
    Co-authored-by: Alex Sanders <alex@sndrs.dev>

--- apps/csnx/.storybook/main.js
+++ apps/csnx/.storybook/main.js
@@ -37,15 +37,15 @@ module.exports = {
 		return {
 			'source-foundations': {
 				title: 'source-foundations',
-				url: 'https://main.diy95zty49kdd.amplifyapp.com',
+				url: 'https://main--635a6aa8070dfdbfdac36f65.chromatic.com',
 			},
 			'source-react-components': {
 				title: 'source-react-components',
-				url: 'https://main.d2zt1j33ncruvj.amplifyapp.com',
+				url: 'https://main--635a6fffacd30d393597c1ff.chromatic.com',
 			},
 			'source-react-components-development-kitchen': {
 				title: 'source-react-components-development-kitchen',
-				url: 'https://main.d25xfnd702qjsd.amplifyapp.com',
+				url: 'https://main--635a7057acd30d393597c208.chromatic.com',
 			},
 		};
 	},
