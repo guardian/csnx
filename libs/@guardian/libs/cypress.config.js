bf999d55409c2780fbd944a1e56c921b0f730d11
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-03-07 10:07:19 +0000

    WIP Co-authored-by: Alex Sanders <alex@sndrs.dev>

--- libs/@guardian/libs/cypress.config.js
+++ libs/@guardian/libs/cypress.config.js
@@ -4,8 +4,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
-		// setupNodeEvents(on, config) {
-		// 	// implement node event listeners here
-		// },
+		supportFile: false,
 	},
 });
