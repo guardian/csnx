39fe01531dce79f20a54fd9322ace79a69b6819d
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-03-07 10:27:39 +0000

    WIP Co-authored-by: Alex Sanders <alex@sndrs.dev>

--- libs/@guardian/libs/cypress.config.js
+++ libs/@guardian/libs/cypress.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-default-export -- that's what cypress likes */
-
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
-		supportFile: false,
+		setupNodeEvents(on, config) {
+			return require('./cypress/plugins/index.js').default(on, config);
+		},
 	},
 });
