f02006f6fb78c37e72f434e6b3475bfad85b16f6
Alex Sanders <alex@sndrs.dev>
2024-03-07 11:50:32 +0000

    add CMP cypress tests
    Co-authored-by: oliverabrahams <ollie.abrahams@guardian.co.uk>
    Co-authored-by: Alex Sanders <alex@sndrs.dev>

---
+++ libs/@guardian/libs/cypress/fixtures/sourcepointConfig.js
@@ -0,0 +1,5 @@
+export const ACCOUNT_ID = 1257;
+export const PRIVACY_MANAGER_CCPA = 540252;
+export const PRIVACY_MANAGER_AUSTRALIA = 540341;
+
+export const ENDPOINT = 'https://cdn.privacy-mgmt.com';
--- libs/@guardian/libs/cypress/support/e2e.js
+++ libs/@guardian/libs/cypress/support/e2e.js
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/e2e.js is processed and
+// This example support/index.js is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -13,4 +13,5 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+// Import commands.js using ES2015 syntax:
 import './commands';
