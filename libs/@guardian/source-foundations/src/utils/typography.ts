e9e1cdcfd5c7a93bb2edc4fc3d6dfe2747e4cd3e
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-03-25 12:17:32 +0000

    Update helper name

--- libs/@guardian/source-foundations/src/utils/typography.ts
+++ libs/@guardian/source-foundations/src/utils/typography.ts
@@ -1,6 +1,6 @@
 const REGEX_FONT_SIZE = /font-size:\s(\d+\.\d+)rem/;
 
-export const typographyPresetPx = (preset: string) => {
+export const presetToPx = (preset: string) => {
 	const matches = preset.match(REGEX_FONT_SIZE);
 	if (matches?.[1]) {
 		const pxVal = parseFloat(matches[1]) * 16;
