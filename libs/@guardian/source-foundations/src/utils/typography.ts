d4e7754b1a37e6c76bbbb2a783f91dfc72a723b0
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-03-25 14:20:36 +0000

    Add comment to conversion function

--- libs/@guardian/source-foundations/src/utils/typography.ts
+++ libs/@guardian/source-foundations/src/utils/typography.ts
@@ -1,10 +1,15 @@
-const REGEX_FONT_SIZE = /font-size:\s(\d+\.\d+)rem/;
+/*
+ * Convert font size in typography preset from default rem value to pixels
+ */
 
 export const presetToPx = (preset: string) => {
+	const REGEX_FONT_SIZE = /font-size:\s(\d+\.\d+)rem/;
+
 	const matches = preset.match(REGEX_FONT_SIZE);
 	if (matches?.[1]) {
 		const pxVal = parseFloat(matches[1]) * 16;
 		return preset.replace(REGEX_FONT_SIZE, `font-size: ${pxVal}px`);
 	}
+
 	return preset;
 };
