70d3c1075d62c97c9cf2557d7799ded8434cb84b
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-04-05 14:52:05 +0100

    Make conversion function types more specific

--- libs/@guardian/source-foundations/src/utils/convert-value.ts
+++ libs/@guardian/source-foundations/src/utils/convert-value.ts
@@ -1,7 +1,8 @@
 export const rootPixelFontSize = 16;
 export const pxToRem = (px: number): number => px / rootPixelFontSize;
-export const pxStringToNumber = (px: string): number => Number(px.slice(0, -2));
-export const pxStringToRem = (px: string): number =>
+export const pxStringToNumber = (px: `${number}px`): number =>
+	Number(px.slice(0, -2));
+export const pxStringToRem = (px: `${number}px`): number =>
 	pxToRem(pxStringToNumber(px));
 export const fontArrayToString = (fonts: readonly string[]): string =>
 	fonts.map((name) => (name.includes(' ') ? `"${name}"` : name)).join(', ');
