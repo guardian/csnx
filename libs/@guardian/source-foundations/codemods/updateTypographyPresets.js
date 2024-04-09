ceb97ff2d81344c3496290e9b9c049c6b43df41c
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-04-09 16:55:00 +0100

    Added all mapping

--- libs/@guardian/source-foundations/codemods/updateTypographyPresets.js
+++ libs/@guardian/source-foundations/codemods/updateTypographyPresets.js
@@ -106,10 +106,179 @@ const existingpresets = [
 	'titlepiece70',
 ];
 
+const typographyApiMapping = {
+	headlineBold17: { fontWeight: 'bold' },
+	headlineBold20: { fontWeight: 'bold' },
+	headlineBold24: { fontWeight: 'bold' },
+	headlineBold28: { fontWeight: 'bold' },
+	headlineBold34: { fontWeight: 'bold' },
+	headlineBold42: { fontWeight: 'bold' },
+	headlineBold50: { fontWeight: 'bold' },
+	headlineLight17: { fontWeight: 'light' },
+	headlineLight20: { fontWeight: 'light' },
+	headlineLight24: { fontWeight: 'light' },
+	headlineLight28: { fontWeight: 'light' },
+	headlineLight34: { fontWeight: 'light' },
+	headlineLight42: { fontWeight: 'light' },
+	headlineLight50: { fontWeight: 'light' },
+	headlineLightItalic17: {
+		fontWeight: 'light',
+		fontStyle: 'italic',
+	},
+	headlineLightItalic20: {
+		fontWeight: 'light',
+		fontStyle: 'italic',
+	},
+	headlineLightItalic24: {
+		fontWeight: 'light',
+		fontStyle: 'italic',
+	},
+	headlineLightItalic28: {
+		fontWeight: 'light',
+		fontStyle: 'italic',
+	},
+	headlineLightItalic34: {
+		fontWeight: 'light',
+		fontStyle: 'italic',
+	},
+	headlineLightItalic42: {
+		fontWeight: 'light',
+		fontStyle: 'italic',
+	},
+	headlineLightItalic50: {
+		fontWeight: 'light',
+		fontStyle: 'italic',
+	},
+	// headlineLightItalic70: () => {},
+	// headlineMedium14: () => {},
+	headlineMedium17: {},
+	headlineMedium20: {},
+	headlineMedium24: {},
+	headlineMedium28: {},
+	headlineMedium34: {},
+	headlineMedium42: {},
+	headlineMedium50: {},
+	// headlineMedium70: () => {},
+	// headlineMediumItalic14: () => {},
+	headlineMediumItalic17: { fontStyle: 'italic' },
+	headlineMediumItalic20: { fontStyle: 'italic' },
+	headlineMediumItalic24: { fontStyle: 'italic' },
+	headlineMediumItalic28: { fontStyle: 'italic' },
+	headlineMediumItalic34: { fontStyle: 'italic' },
+	headlineMediumItalic42: { fontStyle: 'italic' },
+	headlineMediumItalic50: { fontStyle: 'italic' },
+	// headlineMediumItalic70: () => {},
+	textEgyptian14: { lineHeight: 'regular' },
+	textEgyptian15: { lineHeight: 'regular' },
+	textEgyptian17: { lineHeight: 'regular' },
+	textEgyptianBold14: {
+		lineHeight: 'regular',
+		fontWeight: 'bold',
+	},
+	textEgyptianBold15: { lineHeight: 'regular', fontWeight: 'bold' },
+	textEgyptianBold17: {
+		lineHeight: 'regular',
+		fontWeight: 'bold',
+	},
+	textEgyptianBoldItalic14: {
+		lineHeight: 'regular',
+		fontWeight: 'bold',
+		fontStyle: 'italic',
+	},
+	textEgyptianBoldItalic15: {
+		lineHeight: 'regular',
+		fontWeight: 'bold',
+		fontStyle: 'italic',
+	},
+	textEgyptianBoldItalic17: {
+		lineHeight: 'regular',
+		fontWeight: 'bold',
+		fontStyle: 'italic',
+	},
+	textEgyptianItalic14: {
+		lineHeight: 'regular',
+		fontStyle: 'italic',
+	},
+	textEgyptianItalic15: {
+		lineHeight: 'regular',
+		fontStyle: 'italic',
+	},
+	textEgyptianItalic17: {
+		lineHeight: 'regular',
+		fontStyle: 'italic',
+	},
+	textSans12: {},
+	textSans14: {},
+	textSans15: {},
+	textSans17: {},
+	textSans20: {},
+	textSans24: {},
+	textSans28: {},
+	textSans34: {},
+	textSansBold12: { fontWeight: 'bold' },
+	textSansBold14: { fontWeight: 'bold' },
+	textSansBold15: { fontWeight: 'bold' },
+	textSansBold17: { fontWeight: 'bold' },
+	textSansBold20: { fontWeight: 'bold' },
+	textSansBold24: { fontWeight: 'bold' },
+	textSansBold28: { fontWeight: 'bold' },
+	textSansBold34: { fontWeight: 'bold' },
+	textSansItalic12: { fontStyle: 'italic' },
+	textSansItalic14: { fontStyle: 'italic' },
+	textSansItalic15: { fontStyle: 'italic' },
+	textSansItalic17: { fontStyle: 'italic' },
+	textSansItalic20: { fontStyle: 'italic' },
+	textSansItalic24: { fontStyle: 'italic' },
+	textSansItalic28: { fontStyle: 'italic' },
+	textSansItalic34: { fontStyle: 'italic' },
+	titlepiece42: {},
+	titlepiece50: {},
+	titlepiece70: {},
+};
 const lineHeightMapping = {
 	tight: '1.15',
 	regular: '1.3',
 	loose: '1.4',
+};
+
+const getArgumentsFromObjectExpression = (arg) => {
+	const args = [];
+	arg.properties.forEach((prop) => {
+		if (prop.key.type === 'Identifier') {
+			args[prop.key.name] = prop.value.value;
+		}
+	});
+	return args;
+};
+const buildPresetName = (preset, args) => {
+	// Extract the numeric part (size) and the base name from the preset
+	const sizeMatch = preset.match(/\d+$/);
+	const size = sizeMatch ? sizeMatch[0] : '';
+	const baseName = preset.replace(size, '');
+
+	// Initialize variables for fontWeight and fontStyle, if they are provided
+	let fontWeight = args['fontWeight']
+		? args['fontWeight'].charAt(0).toUpperCase() + args['fontWeight'].slice(1)
+		: '';
+	let fontStyle = args['fontStyle']
+		? args['fontStyle'].charAt(0).toUpperCase() + args['fontStyle'].slice(1)
+		: '';
+
+	// Remove `Medium` if font weight is specified
+	if (fontWeight && baseName.includes('Medium')) {
+		baseName.replace('Medium', '');
+	}
+
+	// Reassemble the modified preset name
+	const modifiedPreset = `${baseName}${fontWeight}${fontStyle}${size}`;
+
+	// Check if the modified preset exists in the existing presets
+	if (existingpresets.includes(modifiedPreset)) {
+		return modifiedPreset;
+	} else {
+		// If not, return the original preset
+		return preset;
+	}
 };
 
 module.exports = function (fileInfo, api) {
@@ -129,31 +298,52 @@ module.exports = function (fileInfo, api) {
 					sizeToPresetMapping[expr.callee.object.name][
 						expr.callee.property.name
 					];
-				usedPresets.add(newSize);
 
-				if (expr.arguments.length === 0) return;
+				if (expr.arguments.length === 0) {
+					usedPresets.add(newSize);
+					return;
+				}
 
 				const optionsArg = expr.arguments[0];
 				if (optionsArg.type !== 'ObjectExpression') return;
 
-				const lineHeightProp = optionsArg.properties.find(
-					(prop) => prop.key.name === 'lineHeight',
-				);
-				if (!lineHeightProp) return;
+				const args = getArgumentsFromObjectExpression(optionsArg);
+				const newPresetName = buildPresetName(newSize, args);
+				let addComment = false;
+				let comment = `;
+/** @todo consider not overriding lineHeights */
+`;
+				if (
+					args['fontWeight'] &&
+					typographyApiMapping[newPresetName].fontWeight !== args['fontWeight']
+				) {
+					comment += `font-weight: ${args['fontWeight']};
+					`;
+					addComment = true;
+				}
+				if (
+					args['lineHeight'] &&
+					typographyApiMapping[newPresetName].lineHeight !== args['lineHeight']
+				) {
+					comment += `line-height: ${lineHeightMapping[args['lineHeight']]};
+					`;
+					addComment = true;
+				}
+				if (
+					args['fontStyle'] &&
+					typographyApiMapping[newPresetName].fontStyle !== args['fontStyle']
+				) {
+					comment += `font-style: ${args['fontStyle']};
+					`;
+					addComment = true;
+				}
 
-				const lineHeightKey = lineHeightProp.value.value;
-				const lineHeightValue = lineHeightMapping[lineHeightKey];
-				if (!lineHeightValue) return;
-
+				if (!addComment) return;
 				const nextQuasi = templatePath.node.quasi.quasis[index + 1];
 				if (!nextQuasi) return;
 
-				const commentLine = `;
-/** @todo consider not overriding lineHeights */
-line-height: ${lineHeightValue};`;
-
-				nextQuasi.value.raw = commentLine + nextQuasi.value.raw;
-				nextQuasi.value.cooked = commentLine + nextQuasi.value.cooked;
+				nextQuasi.value.raw = comment + nextQuasi.value.raw;
+				nextQuasi.value.cooked = comment + nextQuasi.value.cooked;
 			}
 		});
 	});
@@ -169,9 +359,16 @@ line-height: ${lineHeightValue};`;
 					},
 				})
 				.forEach((path) => {
-					const newIdentifier = j.identifier(newSize);
+					let args = [];
+					path.node.arguments.forEach((arg) => {
+						if (arg.type === 'ObjectExpression') {
+							args = getArgumentsFromObjectExpression(arg);
+						}
+					});
+					const preset = buildPresetName(newSize, args);
+					const newIdentifier = j.identifier(preset);
 					j(path).replaceWith(newIdentifier);
-					usedPresets.add(newSize);
+					usedPresets.add(preset);
 				});
 		});
 	});
