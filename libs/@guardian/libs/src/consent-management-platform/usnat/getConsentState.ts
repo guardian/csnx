d6ecb06f17b3469630d5edbdd96587dec907111f
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-04-15 10:11:29 +0100

    Account for different apis

--- libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
+++ libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
@@ -1,12 +1,19 @@
-import type { USNATConsentState } from '../types/usnat';
+import { type USNATConsentState } from '../types/usnat';
 import { getGPPData } from './api';
 
-// get the current consent state using the official IAB method
 export const getConsentState: () => Promise<USNATConsentState> = async () => {
+	let doNotSell = true;
 	const gppData = await getGPPData();
 
-	return {
-		// https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Sections/US-National/IAB%20Privacy%E2%80%99s%20National%20Privacy%20Technical%20Specification.md
-		doNotSell: gppData.parsedSections.usnatv1?.SaleOptOut !== 2,
-	};
+	if (gppData.supportedAPIs.length === 0) {
+		doNotSell = true;
+	}
+
+	const supportedAPIs = gppData.supportedAPIs[0]?.split(':')[1]; // This is usually '7:usnatv1', '8:uscav1'
+
+	if (supportedAPIs) {
+		doNotSell = gppData.parsedSections[supportedAPIs]?.SaleOptOut !== 2; //// https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Sections/US-National/IAB%20Privacy%E2%80%99s%20National%20Privacy%20Technical%20Specification.md
+	}
+
+	return { doNotSell: doNotSell };
 };
