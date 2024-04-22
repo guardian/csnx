d93ea6b8c4338bf5fc2b97653f37679f15491f97
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-04-22 08:32:55 +0100

    Update getConsentState.ts

--- libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
+++ libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
@@ -13,7 +13,8 @@ export const getConsentState: () => Promise<USNATConsentState> = async () => {
 
 	if (supportedAPIs) {
 		doNotSell = gppData.parsedSections[supportedAPIs]?.SaleOptOut !== 2; // https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Sections/US-National/IAB%20Privacy%E2%80%99s%20National%20Privacy%20Technical%20Specification.md
+		//0 Not Applicable. SharingOptOutNotice value was not applicable or no notice was provided, 1 Opted Out, 2 Did Not Opt Out
 	}
 
-	return { doNotSell: doNotSell };
+	return { doNotSell };
 };
