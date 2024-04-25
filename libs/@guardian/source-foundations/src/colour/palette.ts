52f7df0f9e1dc57c97eb8f5c392ed4f6b8eedef8
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-04-25 15:59:58 +0100

    update palette export

--- libs/@guardian/source-foundations/src/colour/palette.ts
+++ libs/@guardian/source-foundations/src/colour/palette.ts
@@ -1,24 +1,8 @@
-import { palette as tokens } from '@guardian/design-tokens';
-
 /**
  * [Storybook](https://guardian.github.io/csnx/?path=/docs/source-foundations_palette--palette) •
  * [Design System](https://theguardian.design/2a1e5182b/p/492a30-light-palette)
  *
  * Colour palette organised by type e.g. 'error', or pillar e.g. 'opinion'
  */
-export const palette = {
-	brand: tokens.brand,
-	brandAlt: tokens.brandAlt,
-	neutral: tokens.neutral,
-	error: tokens.error,
-	success: tokens.success,
-	news: tokens.news,
-	opinion: tokens.opinion,
-	sport: tokens.sport,
-	culture: tokens.culture,
-	lifestyle: tokens.lifestyle,
-	labs: tokens.labs,
-	specialReport: tokens.specialReport,
-	specialReportAlt: tokens.specialReportAlt,
-	focus: tokens.focus,
-} as const;
+
+export { palette } from '@guardian/design-tokens';
