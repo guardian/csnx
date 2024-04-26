fed5932d5c93e144ef44311d10cdf673dea8852e
Alex Sanders <alex@sndrs.dev>
2024-04-26 17:46:35 +0100

    make all packages modules, and build with `pkgroll` to local dist

--- libs/@guardian/browserslist-config/dist.test.js
+++
@@ -1,8 +0,0 @@
-const bundleExports = require('../../../dist/libs/@guardian/browserslist-config');
-const srcExports = require('.');
-
-describe('The bundle', () => {
-	it('exports everything it should', () => {
-		expect(srcExports).toEqual(bundleExports);
-	});
-});
