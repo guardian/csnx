307c1f75ad49c135e9d9a639ec13d8f725ef9cb3
Alex Sanders <alex@sndrs.dev>
2024-05-16 16:53:29 +0100

    ignore wireit cache dirs when looking for tests

--- configs/jest.config.js
+++ configs/jest.config.js
@@ -2,7 +2,7 @@
 export const config = {
 	clearMocks: true,
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-
+	testPathIgnorePatterns: ['/node_modules/', '/.wireit/'],
 	transformIgnorePatterns: ['node_modules/(?!@guardian)'],
 	transform: {
 		'^.+\\.[tj]sx?$': [
