20d3de2afed2511835cdcd8deeccaef9131aaf3d
Alex Sanders <alex@sndrs.dev>
2024-05-16 16:19:53 +0100

    create `@guardian/source-development-kitchen`

---
+++ libs/@guardian/source-development-kitchen/.storybook/preview/FocusManagerDecorator.tsx
@@ -0,0 +1,11 @@
+import { FocusStyleManager } from '@guardian/source/foundations';
+import type { Decorator } from '@storybook/react';
+import { useEffect } from 'react';
+
+export const FocusManagerDecorator: Decorator = (storyFn) => {
+	useEffect(() => {
+		FocusStyleManager.onlyShowFocusOnTabs();
+	});
+
+	return <>{storyFn()}</>;
+};
