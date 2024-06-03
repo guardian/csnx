c92cf1466f84bb23722fb64eb9adb77927728cd7
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-06-03 17:42:41 +0100

    Add examples of size options and theming

--- libs/@guardian/source/src/react-components/spinner/Spinner.stories.tsx
+++ libs/@guardian/source/src/react-components/spinner/Spinner.stories.tsx
@@ -1,12 +1,16 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import type { SpinnerProps } from './Spinner';
 import { Spinner } from './Spinner';
+import { palette } from '../../foundations';
 
 const meta: Meta<typeof Spinner> = {
 	title: 'React Components/Spinner',
 	component: Spinner,
-	args: {
-		size: 'medium',
+	argTypes: {
+		size: {
+			options: ['xsmall', 'small', 'medium'],
+			control: { type: 'select' },
+		},
 	},
 };
 
@@ -18,4 +22,44 @@ const Template: StoryFn<typeof Spinner> = (args: SpinnerProps) => (
 
 // *****************************************************************************
 
-export const DefaultTheme: StoryFn<typeof Spinner> = Template.bind({});
+export const XSmallSizeDefaultTheme: StoryFn<typeof Spinner> = Template.bind(
+	{},
+);
+XSmallSizeDefaultTheme.args = {
+	size: 'xsmall',
+};
+
+// *****************************************************************************
+
+export const SmallSizeDefaultTheme: StoryFn<typeof Spinner> = Template.bind({});
+SmallSizeDefaultTheme.args = {
+	size: 'small',
+};
+
+// *****************************************************************************
+
+export const MediumSizeDefaultTheme: StoryFn<typeof Spinner> = Template.bind(
+	{},
+);
+MediumSizeDefaultTheme.args = {
+	size: 'medium',
+};
+
+// *****************************************************************************
+
+export const CustomSizeDefaultTheme: StoryFn<typeof Spinner> = Template.bind(
+	{},
+);
+CustomSizeDefaultTheme.args = {
+	size: 40,
+};
+
+// *****************************************************************************
+
+export const CustomTheme: StoryFn<typeof Spinner> = Template.bind({});
+CustomTheme.args = {
+	theme: {
+		strokeBackground: 'transparent',
+		strokeForeground: palette.neutral[7],
+	},
+};
