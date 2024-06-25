2e6836d4b40b1d947dbb4dddb3b749f56d64fb52
Anna Beddow <anna.beddow@gmail.com>
2024-06-25 11:06:57 +0100

    Add web vital target data

--- libs/@guardian/core-web-vitals/src/@types/CoreWebVitalsPayload.ts
+++ libs/@guardian/core-web-vitals/src/@types/CoreWebVitalsPayload.ts
@@ -1,10 +1,13 @@
 export type CoreWebVitalsPayload = {
 	page_view_id: string | null;
 	browser_id: string | null;
-	fid: null | number;
 	cls: null | number;
+	cls_target: null | string;
+	inp: null | number;
+	inp_target: null | string;
 	lcp: null | number;
+	lcp_target: null | string;
+	fid: null | number;
 	fcp: null | number;
 	ttfb: null | number;
-	inp: null | number;
 };
