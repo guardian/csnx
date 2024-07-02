7b752bb4625b1f20f4373b6d7d55223e4e09ad58
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-07-02 17:55:54 +0100

    Convert accordion stories

--- libs/@guardian/source/src/react-components/accordion/Accordion.stories.tsx
+++ libs/@guardian/source/src/react-components/accordion/Accordion.stories.tsx
@@ -1,78 +1,72 @@
-import type { Meta, StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { palette } from '../../foundations';
-import type { AccordionProps } from './Accordion';
 import { Accordion } from './Accordion';
 import { AccordionRow } from './AccordionRow';
 
 const meta: Meta<typeof Accordion> = {
 	title: 'React Components/Accordion',
 	component: Accordion,
+};
+
+export default meta;
+type Story = StoryObj<typeof Accordion>;
+
+const AccordionTemplate: Story = {
+	render: ({ hideToggleLabel, theme }) => (
+		<Accordion hideToggleLabel={hideToggleLabel} theme={theme}>
+			<AccordionRow label="Collecting from multiple newsagents" theme={theme}>
+				Present your card to a newsagent each time you collect the paper. The
+				newsagent will scan your card and will be reimbursed for each
+				transaction automatically.
+			</AccordionRow>
+			<AccordionRow label="Delivery from your retailer" theme={theme}>
+				Simply give your preferred store / retailer the barcode printed on your
+				subscription letter.
+			</AccordionRow>
+		</Accordion>
+	),
+};
+
+export const WithCTALabelsDefaultTheme: Story = {
+	...AccordionTemplate,
 	args: {
 		hideToggleLabel: false,
 	},
 };
 
-export default meta;
-
-const Template: StoryFn<typeof Accordion> = (args: AccordionProps) => (
-	<Accordion {...args}>
-		<AccordionRow {...args} label="Collecting from multiple newsagents">
-			Present your card to a newsagent each time you collect the paper. The
-			newsagent will scan your card and will be reimbursed for each transaction
-			automatically.
-		</AccordionRow>
-		<AccordionRow {...args} label="Delivery from your retailer">
-			Simply give your preferred store / retailer the barcode printed on your
-			subscription letter.
-		</AccordionRow>
-	</Accordion>
-);
-
-export const WithCTALabelsDefaultTheme: StoryFn<typeof Accordion> =
-	Template.bind({});
-
-// *****************************************************************************
-
-export const WithoutCTALabelsDefaultTheme: StoryFn<typeof Accordion> =
-	Template.bind({});
-WithoutCTALabelsDefaultTheme.args = {
-	hideToggleLabel: true,
-};
-
-// *****************************************************************************
-
-export const WithCTALabelsCustomTheme: StoryFn<typeof Accordion> =
-	Template.bind({});
-WithCTALabelsCustomTheme.args = {
-	theme: {
-		textCta: palette.neutral[86],
-		textLabel: palette.neutral[86],
-		textBody: palette.neutral[86],
-		border: palette.neutral[60],
-		iconFill: palette.neutral[86],
+export const WithoutCTALabelsDefaultTheme: Story = {
+	...AccordionTemplate,
+	args: {
+		hideToggleLabel: true,
 	},
 };
-WithCTALabelsCustomTheme.parameters = {
-	backgrounds: {
-		default: 'background.inverse',
-	},
-};
-// *****************************************************************************
 
-export const WithoutCTALabelsCustomTheme: StoryFn<typeof Accordion> =
-	Template.bind({});
-WithoutCTALabelsCustomTheme.args = {
-	hideToggleLabel: true,
-	theme: {
-		textLabel: palette.neutral[86],
-		textBody: palette.neutral[86],
-		textCta: palette.neutral[86],
-		border: palette.neutral[60],
-		iconFill: palette.neutral[86],
+export const WithCTALabelsCustomTheme: Story = {
+	...AccordionTemplate,
+	args: {
+		...WithCTALabelsDefaultTheme.args,
+		theme: {
+			textCta: palette.neutral[86],
+			textLabel: palette.neutral[86],
+			textBody: palette.neutral[86],
+			border: palette.neutral[60],
+			iconFill: palette.neutral[86],
+		},
+	},
+	parameters: {
+		backgrounds: {
+			default: 'background.inverse',
+		},
 	},
 };
-WithoutCTALabelsCustomTheme.parameters = {
-	backgrounds: {
-		default: 'background.inverse',
+
+export const WithOutCTALabelsCustomTheme: Story = {
+	...AccordionTemplate,
+	args: {
+		...WithCTALabelsCustomTheme.args,
+		hideToggleLabel: true,
+	},
+	parameters: {
+		...WithCTALabelsCustomTheme.parameters,
 	},
 };
