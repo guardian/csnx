59b4e7e32f37566b3219286cfcc25eece4a273b7
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-07-04 19:34:09 +0100

    Convert TextInput stories

--- libs/@guardian/source/src/react-components/text-input/TextInput.stories.tsx
+++ libs/@guardian/source/src/react-components/text-input/TextInput.stories.tsx
@@ -1,21 +1,11 @@
-import type { Meta, StoryFn } from '@storybook/react';
+import type { StoryObj, Meta } from '@storybook/react';
 import { useState } from 'react';
 import { palette } from '../../foundations';
 import { TextInput } from './TextInput';
-import type { TextInputProps } from './TextInput';
 
 const meta: Meta<typeof TextInput> = {
 	title: 'React Components/TextInput',
 	component: TextInput,
-	args: {
-		label: 'Email',
-		optional: false,
-		hideLabel: false,
-		supporting: '',
-		size: 'medium',
-		error: 'undefined',
-		success: 'undefined',
-	},
 	argTypes: {
 		error: {
 			options: ['undefined', 'error'],
@@ -38,231 +28,242 @@ const meta: Meta<typeof TextInput> = {
 };
 
 export default meta;
+type Story = StoryObj<typeof TextInput>;
 
-const Template: StoryFn<typeof TextInput> = (args: TextInputProps) => {
-	const [state, setState] = useState('');
-	return (
-		<TextInput
-			{...args}
-			value={state}
-			onChange={(event) => setState(event.target.value)}
-		/>
-	);
-};
-
-export const DefaultDefaultTheme: StoryFn<typeof TextInput> = Template.bind({});
-
-// *****************************************************************************
-
-export const OptionalDefaultTheme: StoryFn<typeof TextInput> = Template.bind(
-	{},
-);
-OptionalDefaultTheme.args = {
-	optional: true,
-};
-
-// *****************************************************************************
-
-export const HideLabelDefaultTheme: StoryFn<typeof TextInput> = Template.bind(
-	{},
-);
-HideLabelDefaultTheme.args = {
-	hideLabel: true,
-};
-
-// *****************************************************************************
-
-export const SupportingTextDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-SupportingTextDefaultTheme.args = {
-	supporting: 'alex@example.com',
-};
-
-// *****************************************************************************
-
-export const Width30DefaultTheme: StoryFn<typeof TextInput> = Template.bind({});
-Width30DefaultTheme.args = {
-	width: 30,
-	label: 'First name',
-};
-
-// *****************************************************************************
-
-export const Width10DefaultTheme: StoryFn<typeof TextInput> = Template.bind({});
-Width10DefaultTheme.args = {
-	width: 10,
-	label: 'Postcode',
-};
-
-// *****************************************************************************
-
-export const Width4DefaultTheme: StoryFn<typeof TextInput> = Template.bind({});
-Width4DefaultTheme.args = {
-	width: 4,
-	label: 'Year of birth',
-};
-
-// *****************************************************************************
-
-export const ErrorWithMessageDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-ErrorWithMessageDefaultTheme.args = {
-	error: 'error',
-};
-
-// *****************************************************************************
-
-export const SuccessWithMessageDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-SuccessWithMessageDefaultTheme.args = {
-	success: 'success',
-};
-
-// *****************************************************************************
-
-export const ConstraintDefaultTheme: StoryFn<typeof TextInput> = Template.bind(
-	{},
-);
-ConstraintDefaultTheme.args = {
-	label: 'Phone number',
-	pattern: '[0-9]{1,11}',
-	title: 'React Components/11 digit phone number',
-	type: 'tel',
-};
-
-// *****************************************************************************
-
-export const DefaultSmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-DefaultSmallDefaultTheme.args = {
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const OptionalSmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-OptionalSmallDefaultTheme.args = {
-	optional: true,
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const HideLabelSmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-HideLabelSmallDefaultTheme.args = {
-	hideLabel: true,
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const SupportingTextSmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-SupportingTextSmallDefaultTheme.args = {
-	supporting: 'alex@example.com',
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const Width30SmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-Width30SmallDefaultTheme.args = {
-	width: 30,
-	label: 'First name',
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const Width10SmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-Width10SmallDefaultTheme.args = {
-	width: 10,
-	label: 'Postcode',
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const Width4SmallDefaultTheme: StoryFn<typeof TextInput> = Template.bind(
-	{},
-);
-Width4SmallDefaultTheme.args = {
-	width: 4,
-	label: 'Year of birth',
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const ErrorWithMessageSmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-ErrorWithMessageSmallDefaultTheme.args = {
-	error: 'error',
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const SuccessWithMessageSmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-SuccessWithMessageSmallDefaultTheme.args = {
-	success: 'success',
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const ConstraintSmallDefaultTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-ConstraintSmallDefaultTheme.args = {
-	label: 'Phone number',
-	pattern: '[0-9]{1,11}',
-	title: 'React Components/11 digit phone number',
-	type: 'tel',
-	size: 'small',
-};
-
-// *****************************************************************************
-
-export const SupportingTextCustomTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-SupportingTextCustomTheme.args = {
-	supporting: 'alex@example.com',
-	theme: {
-		textUserInput: palette.neutral[86],
-		textLabel: palette.neutral[86],
-		textSupporting: palette.neutral[60],
-		border: palette.neutral[60],
-		backgroundInput: palette.neutral[20],
-	},
-};
-SupportingTextCustomTheme.parameters = {
-	backgrounds: {
-		default: 'background.inverse',
+const TextInputTemplate: Story = {
+	render: (args) => {
+		const [state, setState] = useState('');
+		return (
+			<TextInput
+				{...args}
+				value={state}
+				onChange={(event) => setState(event.target.value)}
+			/>
+		);
 	},
 };
 
-// *****************************************************************************
-
-export const ErrorWithMessageCustomTheme: StoryFn<typeof TextInput> =
-	Template.bind({});
-ErrorWithMessageCustomTheme.args = {
-	error: 'error',
-	theme: {
-		textLabel: palette.neutral[86],
-		textError: palette.error[500],
-		borderError: palette.error[500],
-		backgroundInput: palette.neutral[20],
-	},
-};
-ErrorWithMessageCustomTheme.parameters = {
-	backgrounds: {
-		default: 'background.inverse',
+export const DefaultDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		label: 'Email',
+		optional: false,
+		hideLabel: false,
+		supporting: '',
+		size: 'medium',
+		error: 'undefined',
+		success: 'undefined',
 	},
 };
 
-// *****************************************************************************
+export const OptionalDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		optional: true,
+	},
+};
+
+export const HideLabelDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		hideLabel: true,
+	},
+};
+
+export const SupportingTextDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		supporting: 'alex@example.com',
+	},
+};
+
+export const Width30DefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		width: 30,
+		label: 'First name',
+	},
+};
+
+export const Width10DefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		width: 10,
+		label: 'Postcode',
+	},
+};
+
+export const Width4DefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		width: 4,
+		label: 'Year of birth',
+	},
+};
+
+export const ErrorWithMessageDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		error: 'error',
+	},
+};
+
+export const SuccessWithMessageDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		success: 'success',
+	},
+};
+
+export const ConstraintDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		label: 'Phone number',
+		pattern: '[0-9]{1,11}',
+		title: 'React Components/11 digit phone number',
+		type: 'tel',
+	},
+};
+
+export const DefaultSmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		size: 'small',
+	},
+};
+
+export const OptionalSmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		optional: true,
+		size: 'small',
+	},
+};
+
+export const HideLabelSmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		hideLabel: true,
+		size: 'small',
+	},
+};
+
+export const SupportingTextSmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		supporting: 'alex@example.com',
+		size: 'small',
+	},
+};
+
+export const Width30SmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		width: 30,
+		label: 'First name',
+		size: 'small',
+	},
+};
+
+export const Width10SmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		width: 10,
+		label: 'Postcode',
+		size: 'small',
+	},
+};
+
+export const Width4SmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		width: 4,
+		label: 'Year of birth',
+		size: 'small',
+	},
+};
+
+export const ErrorWithMessageSmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		error: 'error',
+		size: 'small',
+	},
+};
+
+export const SuccessWithMessageSmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		success: 'success',
+		size: 'small',
+	},
+};
+
+export const ConstraintSmallDefaultTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		label: 'Phone number',
+		pattern: '[0-9]{1,11}',
+		title: 'React Components/11 digit phone number',
+		type: 'tel',
+		size: 'small',
+	},
+};
+
+export const SupportingTextCustomTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		supporting: 'alex@example.com',
+		theme: {
+			textUserInput: palette.neutral[86],
+			textLabel: palette.neutral[86],
+			textSupporting: palette.neutral[60],
+			border: palette.neutral[60],
+			backgroundInput: palette.neutral[20],
+		},
+	},
+	parameters: {
+		backgrounds: {
+			default: 'background.inverse',
+		},
+	},
+};
+
+export const ErrorWithMessageCustomTheme: Story = {
+	...TextInputTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		error: 'error',
+		theme: {
+			textLabel: palette.neutral[86],
+			textError: palette.error[500],
+			borderError: palette.error[500],
+			backgroundInput: palette.neutral[20],
+		},
+	},
+	parameters: {
+		backgrounds: {
+			default: 'background.inverse',
+		},
+	},
+};
