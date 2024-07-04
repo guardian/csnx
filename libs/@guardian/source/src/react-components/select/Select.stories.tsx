c92abeb25680f3eeab222ffab4e785efd43d0444
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2024-07-04 19:25:26 +0100

    Convert Select stories

--- libs/@guardian/source/src/react-components/select/Select.stories.tsx
+++ libs/@guardian/source/src/react-components/select/Select.stories.tsx
@@ -1,7 +1,6 @@
-import type { Meta, StoryFn } from '@storybook/react';
+import type { StoryObj, Meta } from '@storybook/react';
 import { palette } from '../../foundations';
 import { Option } from './Option';
-import type { SelectProps } from './Select';
 import { Select } from './Select';
 
 const meta: Meta<typeof Select> = {
@@ -26,6 +25,23 @@ const meta: Meta<typeof Select> = {
 			control: { type: 'radio' },
 		},
 	},
+};
+
+export default meta;
+type Story = StoryObj<typeof Select>;
+
+const SelectTemplate: Story = {
+	render: (args) => (
+		<Select {...args}>
+			<Option value="">Select a state</Option>
+			<Option value="al">Alabama</Option>
+			<Option value="ca">California</Option>
+		</Select>
+	),
+};
+
+export const DefaultDefaultTheme: Story = {
+	...SelectTemplate,
 	args: {
 		label: 'State',
 		optional: false,
@@ -36,118 +52,102 @@ const meta: Meta<typeof Select> = {
 	},
 };
 
-export default meta;
-
-const Template: StoryFn<typeof Select> = (args: SelectProps) => (
-	<Select {...args}>
-		<Option value="">Select a state</Option>
-		<Option value="al">Alabama</Option>
-		<Option value="ca">California</Option>
-	</Select>
-);
-
-// *****************************************************************************
-
-export const DefaultDefaultTheme: StoryFn<typeof Select> = Template.bind({});
-
-// *****************************************************************************
-
-export const VisuallyHideLabelDefaultTheme: StoryFn<typeof Select> =
-	Template.bind({});
-VisuallyHideLabelDefaultTheme.args = {
-	hideLabel: true,
-};
-
-// *****************************************************************************
-
-export const OptionalDefaultTheme: StoryFn<typeof Select> = Template.bind({});
-OptionalDefaultTheme.args = {
-	optional: true,
-};
-
-// *****************************************************************************
-
-export const ErrorWithMessageDefaultTheme: StoryFn<typeof Select> =
-	Template.bind({});
-ErrorWithMessageDefaultTheme.args = {
-	error: 'error',
-};
-
-// *****************************************************************************
-
-export const SuccessWithMessageDefaultTheme: StoryFn<typeof Select> =
-	Template.bind({});
-SuccessWithMessageDefaultTheme.args = {
-	success: 'success',
-};
-
-// *****************************************************************************
-
-export const SupportingTextDefaultTheme: StoryFn<typeof Select> = Template.bind(
-	{},
-);
-SupportingTextDefaultTheme.args = {
-	supporting: 'Leave blank if you are not within the US',
-};
-
-// *****************************************************************************
-
-export const SupportingSuccessTextDefaultTheme: StoryFn<typeof Select> =
-	Template.bind({});
-SupportingSuccessTextDefaultTheme.args = {
-	supporting: 'Leave blank if you are not within the US',
-
-	success: 'success',
-};
-
-// *****************************************************************************
-
-export const SupportingErrorTextDefaultTheme: StoryFn<typeof Select> =
-	Template.bind({});
-SupportingErrorTextDefaultTheme.args = {
-	supporting: 'Leave blank if you are not within the US',
-	error:
-		'Please select your home state. This service is unavailable outside of the US.',
-};
-
-// *****************************************************************************
-
-export const SupportingTextCustomTheme: StoryFn<typeof Select> = Template.bind(
-	{},
-);
-SupportingTextCustomTheme.args = {
-	supporting: 'Leave blank if you are not within the US',
-	theme: {
-		textLabel: palette.neutral[86],
-		textSupporting: palette.neutral[60],
-		textUserInput: palette.neutral[86],
-		iconFill: palette.neutral[86],
-		border: palette.neutral[60],
-		backgroundInput: palette.neutral[20],
-	},
-};
-SupportingTextCustomTheme.parameters = {
-	backgrounds: {
-		default: 'background.inverse',
+export const VisuallyHideLabelDefaultTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		hideLabel: true,
 	},
 };
 
-// *****************************************************************************
-
-export const ErrorWithMessageCustomTheme: StoryFn<typeof Select> =
-	Template.bind({});
-ErrorWithMessageCustomTheme.args = {
-	error: 'error',
-	theme: {
-		textLabel: palette.neutral[86],
-		textError: palette.error[500],
-		iconFill: palette.error[500],
-		borderError: palette.error[500],
-		backgroundInput: palette.neutral[20],
+export const OptionalDefaultTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		optional: true,
 	},
 };
-ErrorWithMessageCustomTheme.parameters = {
-	backgrounds: {
-		default: 'background.inverse',
+
+export const ErrorWithMessageDefaultTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		error: 'error',
+	},
+};
+
+export const SuccessWithMessageDefaultTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		success: 'success',
+	},
+};
+
+export const SupportingTextDefaultTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		supporting: 'Leave blank if you are not within the US',
+	},
+};
+
+export const SupportingSuccessTextDefaultTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		supporting: 'Leave blank if you are not within the US',
+		success: 'success',
+	},
+};
+
+export const SupportingErrorTextDefaultTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		supporting: 'Leave blank if you are not within the US',
+		error:
+			'Please select your home state. This service is unavailable outside of the US.',
+	},
+};
+
+export const SupportingTextCustomTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		supporting: 'Leave blank if you are not within the US',
+		theme: {
+			textLabel: palette.neutral[86],
+			textSupporting: palette.neutral[60],
+			textUserInput: palette.neutral[86],
+			iconFill: palette.neutral[86],
+			border: palette.neutral[60],
+			backgroundInput: palette.neutral[20],
+		},
+	},
+	parameters: {
+		backgrounds: {
+			default: 'background.inverse',
+		},
+	},
+};
+
+export const ErrorWithMessageCustomTheme: Story = {
+	...SelectTemplate,
+	args: {
+		...DefaultDefaultTheme.args,
+		error: 'error',
+		theme: {
+			textLabel: palette.neutral[86],
+			textError: palette.error[500],
+			iconFill: palette.error[500],
+			borderError: palette.error[500],
+			backgroundInput: palette.neutral[20],
+		},
+	},
+	parameters: {
+		backgrounds: {
+			default: 'background.inverse',
+		},
 	},
 };
