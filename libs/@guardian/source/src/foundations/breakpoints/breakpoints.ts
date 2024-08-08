7c16f8fbbfe21c0d26559100ae314c24e978e15e
Alex Sanders <alex@sndrs.dev>
2024-08-08 16:16:54 +0100

    use the breakpoints

--- libs/@guardian/source/src/foundations/breakpoints/breakpoints.ts
+++ libs/@guardian/source/src/foundations/breakpoints/breakpoints.ts
@@ -1,15 +1,1 @@
-import { breakpoint } from '@guardian/design-tokens';
-import { pxStringToNumber } from '../utils/convert-value';
-
-export type Breakpoint = keyof typeof breakpoint;
-
-export const breakpoints = {
-	mobile: pxStringToNumber(breakpoint.mobile),
-	mobileMedium: pxStringToNumber(breakpoint.mobileMedium),
-	mobileLandscape: pxStringToNumber(breakpoint.mobileLandscape),
-	phablet: pxStringToNumber(breakpoint.phablet),
-	tablet: pxStringToNumber(breakpoint.tablet),
-	desktop: pxStringToNumber(breakpoint.desktop),
-	leftCol: pxStringToNumber(breakpoint.leftCol),
-	wide: pxStringToNumber(breakpoint.wide),
-} as const;
+export { breakpoint, type Breakpoint } from '../__generated__/breakpoints';
