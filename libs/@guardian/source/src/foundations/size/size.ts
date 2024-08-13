102eacc3600c684974750ecaed9119e9941146d4
Alex Sanders <alex@sndrs.dev>
2024-08-13 17:34:22 +0100

    fix jsdoc

--- libs/@guardian/source/src/foundations/size/size.ts
+++ libs/@guardian/source/src/foundations/size/size.ts
@@ -1,22 +1,21 @@
-/**
- * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#global-size-values) •
- * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
- *
- * May be used for call to action buttons and user input fields.
- */
-export { size } from '../__generated__/size';
+export {
+	/**
+	 * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#global-size-values) •
+	 * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
+	 *
+	 * May be used for call to action buttons and user input fields.
+	 */ size,
+} from '../__generated__/size';
 
-/**
- * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#global-size-values) •
- * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
- *
- * May be used for call to action buttons and user input fields.
- *
- ** `remSize.medium` -> 2.75rem
- ** `remSize.small` -> 2.25rem
- ** `remSize.xsmall` -> 1.5rem
- */
-export { remSize } from '../__generated__/size';
+export {
+	/**
+	 * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#global-size-values) •
+	 * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
+	 *
+	 * May be used for call to action buttons and user input fields.
+	 *
+	 */ remSize,
+} from '../__generated__/size';
 
 /*
    We attempt to use these values for icons within Source components.
@@ -27,58 +26,58 @@ export { remSize } from '../__generated__/size';
 export { iconSize } from '../__generated__/size';
 export { remIconSize } from '../__generated__/size';
 
-/**
- * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#tokens) •
- * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
- *
- * Height tokens expressed in px
- *
- ** `height.ctaMedium`: call to action buttons and links
- ** `height.ctaSmall`: secondary calls to action
- ** `height.ctaXsmall`: calls to action where there is very limited space
- ** `height.inputMedium`: text input fields, radio and checkbox labels
- ** `height.inputXsmall`: checkables such as checkboxes or radio buttons
- ** `height.iconMedium`
- ** `height.iconSmall`
- ** `height.iconXsmall`
- */
-export { height } from '../__generated__/size';
+export {
+	/**
+	 * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#tokens) •
+	 * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
+	 *
+	 * Height tokens expressed in px
+	 *
+	 ** `height.ctaMedium`: call to action buttons and links
+	 ** `height.ctaSmall`: secondary calls to action
+	 ** `height.ctaXsmall`: calls to action where there is very limited space
+	 ** `height.iconMedium`
+	 ** `height.iconSmall`
+	 ** `height.iconXsmall`
+	 ** `height.inputMedium`: text input fields, radio and checkbox labels
+	 ** `height.inputXsmall`: checkables such as checkboxes or radio buttons
+	 */ height,
+} from '../__generated__/size';
 
-/**
- * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#tokens) •
- * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
- *
- * Height tokens expressed in rem
- *
- ** `remHeight.ctaMedium`: call to action buttons and links
- ** `remHeight.ctaSmall`: secondary calls to action
- ** `remHeight.ctaXsmall`: calls to action where there is very limited space
- ** `remHeight.inputMedium`: text input fields, radio and checkbox labels
- ** `remHeight.inputXsmall`: checkables such as checkboxes or radio buttons
- ** `remHeight.iconMedium`
- ** `remHeight.iconSmall`
- ** `remHeight.iconXsmall`
- */
-export { remHeight } from '../__generated__/size';
+export {
+	/**
+	 * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#tokens) •
+	 * [Design System](https://theguardian.design/2a1e5182b/p/24a3ec-size/t/329aef)
+	 *
+	 * Height tokens expressed in rem
+	 *
+	 ** `remHeight.ctaMedium`: call to action buttons and links
+	 ** `remHeight.ctaSmall`: secondary calls to action
+	 ** `remHeight.ctaXsmall`: calls to action where there is very limited space
+	 ** `remHeight.iconMedium`
+	 ** `remHeight.iconSmall`
+	 ** `remHeight.iconXsmall`
+	 ** `remHeight.inputMedium`: text input fields, radio and checkbox labels
+	 ** `remHeight.inputXsmall`: checkables such as checkboxes or radio buttons
+	 */ remHeight,
+} from '../__generated__/size';
 
-/**
- * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#tokens) •
- * [Design System](https://theguardian.design/2a1e5182b/p/00ddcb-tokens/t/797660)
- *
- * Width tokens expressed in px
- *
- ** `width.ctaMedium`: medium height call to action buttons and links
- ** `width.ctaSmall`: small height call to action buttons and links
- ** `width.ctaXsmall`: xsmall height call to action buttons and links
- ** `width.inputMedium`: text input fields, radio and checkbox labels
- ** `width.inputXsmall`: checkables such as checkboxes or radio buttons
- ** `width.inputMedium`
- ** `width.inputXsmall`: checkables such as checkboxes or radio buttons
- ** `width.iconMedium`
- ** `width.iconSmall`
- ** `width.iconXsmall`
- */
-export { width } from '../__generated__/size';
+export {
+	/**
+	 * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#tokens) •
+	 * [Design System](https://theguardian.design/2a1e5182b/p/00ddcb-tokens/t/797660)
+	 *
+	 * Width tokens expressed in px
+	 *
+	 ** `width.ctaMedium`: medium height call to action buttons and links
+	 ** `width.ctaSmall`: small height call to action buttons and links
+	 ** `width.ctaXsmall`: xsmall height call to action buttons and links
+	 ** `width.iconMedium`
+	 ** `width.iconSmall`
+	 ** `width.iconXsmall`
+	 ** `width.inputXsmall`: checkables such as checkboxes or radio buttons
+	 */ width,
+} from '../__generated__/size';
 
 /**
  * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-size--page#tokens) •
@@ -89,12 +88,9 @@ export { width } from '../__generated__/size';
  ** `remWidth.ctaMedium`: medium height call to action buttons and links
  ** `remWidth.ctaSmall`: small height call to action buttons and links
  ** `remWidth.ctaXsmall`: xsmall height call to action buttons and links
- ** `remWidth.inputMedium`: text input fields, radio and checkbox labels
- ** `remWidth.inputXsmall`: checkables such as checkboxes or radio buttons
- ** `remWidth.inputMedium`
- ** `remWidth.inputXsmall`: checkables such as checkboxes or radio buttons
  ** `remWidth.iconMedium`
  ** `remWidth.iconSmall`
  ** `remWidth.iconXsmall`
+ ** `remWidth.inputXsmall`: checkables such as checkboxes or radio buttons
  */
 export { remWidth } from '../__generated__/size';
--- libs/@guardian/source/src/foundations/space/space.ts
+++ libs/@guardian/source/src/foundations/space/space.ts
@@ -1,8 +1,11 @@
-/**
- * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-space--page) •
- * [Design System](https://theguardian.design/2a1e5182b/p/05f835-space)
- *
- * Can be applied to margin or padding properties, vertically or horizontally.
- *
- */
-export { remSpace, space } from '../__generated__/space';
+export {
+	/**
+	 * [Storybook](https://guardian.github.io/storybooks/?path=/docs/source_foundations-space--page) •
+	 * [Design System](https://theguardian.design/2a1e5182b/p/05f835-space)
+	 *
+	 * Can be applied to margin or padding properties, vertically or horizontally.
+	 *
+	 */
+	space,
+	remSpace,
+} from '../__generated__/space';
