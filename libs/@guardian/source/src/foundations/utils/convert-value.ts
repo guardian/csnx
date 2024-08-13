7805d16b34c4d662806f412d41a855146e76f24c
Alex Sanders <alex@sndrs.dev>
2024-08-13 14:06:46 +0100

    Aligns values of `rem*` exports from `source` (#1643)
    align values of `rem*` exports from source
    
    Brings `remSize`, `remIconSize`, `remHeight` and `remWidth` into line with `remSpace`, by changing their values from numbers (of rems) to strings with `rem` units.

--- libs/@guardian/source/src/foundations/utils/convert-value.ts
+++ libs/@guardian/source/src/foundations/utils/convert-value.ts
@@ -1,9 +1,16 @@
+export type RemString<N extends number = number> = `${N}rem`;
+export type PxString<N extends number = number> = `${N}px`;
+
 export const rootPixelFontSize = 16;
 
-export const pxToRem = (px: number): number => px / rootPixelFontSize;
-export const pxStringToRem = (px: `${number}px`): number =>
+export const pxToRem = (px: number): RemString =>
+	`${px / rootPixelFontSize}rem`;
+
+export const pxStringToRem = (px: PxString): RemString =>
 	pxToRem(pxStringToNumber(px));
-export const pxStringToNumber = <N extends number>(px: `${N}px`): N =>
+
+export const pxStringToNumber = <N extends number>(px: PxString<N>): N =>
 	Number(px.slice(0, -2)) as N;
+
 export const fontArrayToString = (fonts: readonly string[]): string =>
 	fonts.map((name) => (name.includes(' ') ? `"${name}"` : name)).join(', ');
