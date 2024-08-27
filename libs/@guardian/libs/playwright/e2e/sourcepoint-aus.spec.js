9420bb79c90c179dc9c727ad9f58fc77c6135337
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-08-27 16:27:29 +0100

    Use actual Sourcepoint config variables

--- libs/@guardian/libs/playwright/e2e/sourcepoint-aus.spec.js
+++ libs/@guardian/libs/playwright/e2e/sourcepoint-aus.spec.js
@@ -1,9 +1,6 @@
 import { expect, test } from '@playwright/test';
-import {
-	ACCOUNT_ID,
-	ENDPOINT,
-	PRIVACY_MANAGER_AUSTRALIA,
-} from '../fixtures/sourcepointConfig';
+import { ACCOUNT_ID, ENDPOINT } from '../fixtures/sourcepointConfig';
+import { PRIVACY_MANAGER_AUSTRALIA } from '../../src/consent-management-platform/lib/sourcepointConfig';
 
 const iframePrivacyManager = `#sp_message_iframe_${PRIVACY_MANAGER_AUSTRALIA}`;
 
