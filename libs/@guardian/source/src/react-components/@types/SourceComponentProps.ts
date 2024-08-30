47fb9d394063d4668708b3093e126fffc0c2860f
Alex Sanders <alex@sndrs.dev>
2024-08-30 11:20:12 +0100

    Disallow use of emotion's `css` prop in Source components.
    It binds us too heavily to emotion.

--- libs/@guardian/source/src/react-components/@types/SourceComponentProps.ts
+++ libs/@guardian/source/src/react-components/@types/SourceComponentProps.ts
@@ -6,4 +6,14 @@ export interface SourceComponentProps {
 	 * Override component styles by passing in the result of [emotion's `css` function/prop](https://emotion.sh/docs/introduction).
 	 */
 	cssOverrides?: SerializedStyles | SerializedStyles[];
+	/**
+	 * Do not use `css` on Source components, use `cssOverrides` instead.
+	 *
+	 * **Why?**
+	 *
+	 * `css` is an emotion prop, and while
+	 * Source components _are_ emotion components, it's an internal implementation detail that is liable to break/change
+	 * at any point.
+	 */
+	css?: never;
 }
