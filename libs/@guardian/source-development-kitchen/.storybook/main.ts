760e391f84fa9afc1d25661f833f42d6ead6544f
Alex Sanders <alex@sndrs.dev>
2024-09-04 18:29:04 +0100

    lint fixes

--- libs/@guardian/source-development-kitchen/.storybook/main.ts
+++ libs/@guardian/source-development-kitchen/.storybook/main.ts
@@ -6,5 +6,4 @@ const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 };
 
-/* eslint-disable-next-line import/no-default-export -- it's the storybook way */
 export default config;
