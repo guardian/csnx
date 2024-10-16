5f8fa1dfa8872f2d9be09b6dabc645e25ac493fd
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-10-16 10:58:09 +0100

    Check GPC for doNotSell

--- libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
+++ libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
@@ -8,8 +8,11 @@ export const getConsentState: () => Promise<USNATConsentState> = async () => {
 	const supportedAPIs = gppData.supportedAPIs[0]?.split(':')[1]; // E.G: '7:usnatv1', '8:uscav1'
 
 	if (supportedAPIs) {
-		doNotSell = gppData.parsedSections[supportedAPIs]?.SaleOptOut !== 2; // https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Sections/US-National/IAB%20Privacy%E2%80%99s%20National%20Privacy%20Technical%20Specification.md
 		//0 Not Applicable. SharingOptOutNotice value was not applicable or no notice was provided, 1 Opted Out, 2 Did Not Opt Out
+		doNotSell =
+			gppData.parsedSections[supportedAPIs]?.SaleOptOut !== 2 ||
+			gppData.parsedSections[supportedAPIs].Gpc;
+		// https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Sections/US-National/IAB%20Privacy%E2%80%99s%20National%20Privacy%20Technical%20Specification.md
 	}
 
 	return {
