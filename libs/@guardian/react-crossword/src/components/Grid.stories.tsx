7c3b182645df83790b8bcf965ee6d74f5de9187b
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-11-07 15:54:10 +0000

    fix grid stories

--- libs/@guardian/react-crossword/src/components/Grid.stories.tsx
+++ libs/@guardian/react-crossword/src/components/Grid.stories.tsx
@@ -9,7 +9,7 @@ import { special } from '../stories/special';
 import { speedy } from '../stories/speedy';
 import { weekend } from '../stories/weekend';
 import { defaultTheme } from '../theme';
-import { getCellsMap } from '../utils/getCells';
+import { getCells } from '../utils/getCells';
 import type { GridProps } from './Grid';
 import { Grid } from './Grid';
 
@@ -26,63 +26,63 @@ const Template: StoryFn<typeof Grid> = (args: GridProps) => {
 
 export const Cryptic: StoryFn<typeof Grid> = Template.bind({});
 Cryptic.args = {
-	cellsMap: getCellsMap(cryptic),
+	cells: getCells(cryptic),
 	rows: cryptic.dimensions.rows,
 	cols: cryptic.dimensions.cols,
 };
 
 export const Everyman: StoryFn<typeof Grid> = Template.bind({});
 Everyman.args = {
-	cellsMap: getCellsMap(everyman),
+	cells: getCells(everyman),
 	rows: everyman.dimensions.rows,
 	cols: everyman.dimensions.cols,
 };
 
 export const Prize: StoryFn<typeof Grid> = Template.bind({});
 Prize.args = {
-	cellsMap: getCellsMap(prize),
+	cells: getCells(prize),
 	rows: prize.dimensions.rows,
 	cols: prize.dimensions.cols,
 };
 
 export const Quick: StoryFn<typeof Grid> = Template.bind({});
 Quick.args = {
-	cellsMap: getCellsMap(quick),
+	cells: getCells(quick),
 	rows: quick.dimensions.rows,
 	cols: quick.dimensions.cols,
 };
 
 export const QuickCryptic: StoryFn<typeof Grid> = Template.bind({});
 QuickCryptic.args = {
-	cellsMap: getCellsMap(quickCryptic),
+	cells: getCells(quickCryptic),
 	rows: quickCryptic.dimensions.rows,
 	cols: quickCryptic.dimensions.cols,
 };
 
 export const Quiptic: StoryFn<typeof Grid> = Template.bind({});
 Quiptic.args = {
-	cellsMap: getCellsMap(quiptic),
+	cells: getCells(quiptic),
 	rows: quiptic.dimensions.rows,
 	cols: quiptic.dimensions.cols,
 };
 
 export const Special: StoryFn<typeof Grid> = Template.bind({});
 Special.args = {
-	cellsMap: getCellsMap(special),
+	cells: getCells(special),
 	rows: special.dimensions.rows,
 	cols: special.dimensions.cols,
 };
 
 export const Speedy: StoryFn<typeof Grid> = Template.bind({});
 Speedy.args = {
-	cellsMap: getCellsMap(speedy),
+	cells: getCells(speedy),
 	rows: speedy.dimensions.rows,
 	cols: speedy.dimensions.cols,
 };
 
 export const Weekend: StoryFn<typeof Grid> = Template.bind({});
 Weekend.args = {
-	cellsMap: getCellsMap(weekend),
+	cells: getCells(weekend),
 	rows: weekend.dimensions.rows,
 	cols: weekend.dimensions.cols,
 };
