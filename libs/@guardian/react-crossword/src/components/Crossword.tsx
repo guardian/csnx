943a8f3c4d01316c872187b4da0c1d9bb94afd31
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-11-14 16:00:32 +0000

    change name of grid area to crossword-grid

--- libs/@guardian/react-crossword/src/components/Crossword.tsx
+++ libs/@guardian/react-crossword/src/components/Crossword.tsx
@@ -362,15 +362,15 @@ export const Crossword = ({ theme: userTheme, data }: CrosswordProps) => {
 				@media (min-width: ${theme.wideBreakpoint}px) {
 					grid-template-columns: minmax(auto, ${gridWidth}px) 1fr;
 					grid-template-areas:
-						'grid 		clues'
-						'controls clues'
-						'. 				clues';
+						'crossword-grid clues'
+						'controls 			clues'
+						'. 							clues';
 				}
 			`}
 		>
 			<div
 				css={css`
-					grid-area: grid;
+					grid-area: crossword-grid;
 					text-align: center;
 				`}
 			>
