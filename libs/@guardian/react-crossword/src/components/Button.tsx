dc9e1bad51a538889be29bbf2434e9d46bb75322
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-11-18 09:29:23 +0000

    cleanup react memo

--- libs/@guardian/react-crossword/src/components/Button.tsx
+++ libs/@guardian/react-crossword/src/components/Button.tsx
@@ -7,38 +7,38 @@ type ButtonProps = SourceButtonProps & {
 	requireConfirmation?: boolean;
 };
 
-const ButtonComponent = ({
-	children,
-	requireConfirmation = false,
-	onSuccess,
-	...props
-}: ButtonProps) => {
-	const [isConfirming, setIsConfirming] = useState<boolean>(false);
-	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+export const Button = memo(
+	({
+		children,
+		requireConfirmation = false,
+		onSuccess,
+		...props
+	}: ButtonProps) => {
+		const [isConfirming, setIsConfirming] = useState<boolean>(false);
+		const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-	const onClick = () => {
-		if (!requireConfirmation) {
+		const onClick = () => {
+			if (!requireConfirmation) {
+				onSuccess();
+				return;
+			}
+			if (!isConfirming) {
+				setIsConfirming(true);
+				timeoutRef.current = setTimeout(() => setIsConfirming(false), 3000);
+				return;
+			}
+			if (timeoutRef.current) {
+				clearTimeout(timeoutRef.current);
+			}
 			onSuccess();
-			return;
-		}
-		if (!isConfirming) {
-			setIsConfirming(true);
-			timeoutRef.current = setTimeout(() => setIsConfirming(false), 3000);
-			return;
-		}
-		if (timeoutRef.current) {
-			clearTimeout(timeoutRef.current);
-		}
-		onSuccess();
-		setIsConfirming(false);
-	};
+			setIsConfirming(false);
+		};
 
-	return (
-		<SourceButton onClick={onClick} size="xsmall" {...props}>
-			{isConfirming && 'Confirm '}
-			{children}
-		</SourceButton>
-	);
-};
-
-export const Button = memo(ButtonComponent);
+		return (
+			<SourceButton onClick={onClick} size="xsmall" {...props}>
+				{isConfirming && 'Confirm '}
+				{children}
+			</SourceButton>
+		);
+	},
+);
--- libs/@guardian/react-crossword/src/components/Cell.tsx
+++ libs/@guardian/react-crossword/src/components/Cell.tsx
@@ -12,68 +12,60 @@ export type CellProps = {
 	isHighlighted?: boolean;
 };
 
-export const CellComponent = ({
-	data,
-	x,
-	y,
-	guess = '',
-	theme,
-	isFocused,
-	isHighlighted,
-}: CellProps) => {
-	let border = {};
-	if (isFocused) {
-		// set rect stroke and stroke-width
-		border = {
-			stroke: theme.focusBorder,
-			strokeWidth: 2,
-			rx: 2,
-			ry: 2,
-		};
-	}
+export const Cell = memo(
+	({ data, x, y, guess = '', theme, isFocused, isHighlighted }: CellProps) => {
+		let border = {};
+		if (isFocused) {
+			// set rect stroke and stroke-width
+			border = {
+				stroke: theme.focusBorder,
+				strokeWidth: 2,
+				rx: 2,
+				ry: 2,
+			};
+		}
 
-	const backgroundColor = isUndefined(data.group)
-		? 'transparent'
-		: isHighlighted
-			? theme.focus
-			: theme.foreground;
+		const backgroundColor = isUndefined(data.group)
+			? 'transparent'
+			: isHighlighted
+				? theme.focus
+				: theme.foreground;
 
-	return (
-		<g data-x={data.x} data-y={data.y}>
-			<rect
-				x={x}
-				y={y}
-				width={theme.cellSize}
-				height={theme.cellSize}
-				fill={backgroundColor}
-				{...border}
-			/>
-			{data.number && (
+		return (
+			<g data-x={data.x} data-y={data.y}>
+				<rect
+					x={x}
+					y={y}
+					width={theme.cellSize}
+					height={theme.cellSize}
+					fill={backgroundColor}
+					{...border}
+				/>
+				{data.number && (
+					<text
+						x={x}
+						y={y}
+						dy={8}
+						height={theme.cellSize}
+						width={theme.cellSize}
+						fill={theme.text}
+						style={{ fontSize: 10 }}
+					>
+						{data.number}
+					</text>
+				)}
 				<text
 					x={x}
 					y={y}
-					dy={8}
-					height={theme.cellSize}
-					width={theme.cellSize}
-					fill={theme.text}
-					style={{ fontSize: 10 }}
+					dx={theme.cellSize / 2}
+					dy={theme.cellSize / 1.7}
+					textAnchor="middle"
+					dominantBaseline="middle"
+					style={{ fontSize: theme.cellSize * 0.7 }}
 				>
-					{data.number}
+					{guess}
 				</text>
-			)}
-			<text
-				x={x}
-				y={y}
-				dx={theme.cellSize / 2}
-				dy={theme.cellSize / 1.7}
-				textAnchor="middle"
-				dominantBaseline="middle"
-				style={{ fontSize: theme.cellSize * 0.7 }}
-			>
-				{guess}
-			</text>
-		</g>
-	);
-};
-
-export const Cell = memo(CellComponent);
+			</g>
+		);
+	},
+);
