ed6da121f436fc3a27f56dc7584afad3adde7380
Ravi <7014230+arelra@users.noreply.github.com>
2024-11-21 13:32:51 +0000

    Read gppData.parsedSection[0] directly to set the doNotSell signal
    Co-authored-by: Ashish Puliyel <ashish.puliyel@theguardian.com>

--- libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
+++ libs/@guardian/libs/src/consent-management-platform/usnat/getConsentState.ts
@@ -1,31 +1,15 @@
 import { type USNATConsentState } from '../types/usnat';
 import { getGPPData } from './api';
 
-const usnatStates = ['usnat', 'usca', 'usva', 'usco', 'usut', 'usct'];
-
-const getSupportedAPIv1 = (
-	supportedAPI: string | undefined,
-): string | undefined => {
-	if (supportedAPI && usnatStates.includes(supportedAPI)) {
-		return supportedAPI.includes('v1') ? supportedAPI : `${supportedAPI}v1`;
-	}
-	return supportedAPI;
-};
-
 export const getConsentState: () => Promise<USNATConsentState> = async () => {
 	let doNotSell = false; // Opt-Out
 	const gppData = await getGPPData();
+	const supportedAPI = gppData.parsedSections[0];
 
-	const supportedAPI = gppData.supportedAPIs[0]?.split(':')[1]; // E.G: '7:usnatv1', '8:uscav1'
-	// Temporary fix 21/11/2024
-	const supportedAPIv1 = getSupportedAPIv1(supportedAPI);
-
-	if (supportedAPIv1) {
+	if (supportedAPI) {
 		// https://github.com/InteractiveAdvertisingBureau/Global-Privacy-Platform/blob/main/Sections/US-National/IAB%20Privacy%E2%80%99s%20National%20Privacy%20Technical%20Specification.md
 		// 0 Not Applicable. SharingOptOutNotice value was not applicable or no notice was provided, 1 Opted Out, 2 Did Not Opt Out
-		doNotSell =
-			gppData.parsedSections[supportedAPIv1]?.SaleOptOut !== 2 ||
-			gppData.parsedSections[supportedAPIv1].Gpc;
+		doNotSell = supportedAPI.SaleOptOut !== 2 || supportedAPI.Gpc;
 	}
 
 	return {
