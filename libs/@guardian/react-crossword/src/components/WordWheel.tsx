00264632e1a58b251a9b87f267d7aa14368aabc4
oliverabrahams <ollie.abrahams@guardian.co.uk>
2024-11-29 16:21:18 +0000

    refactor word wheel

--- libs/@guardian/react-crossword/src/components/WordWheel.tsx
+++ libs/@guardian/react-crossword/src/components/WordWheel.tsx
@@ -3,6 +3,52 @@ import { textSans17, textSansBold17 } from '@guardian/source/foundations';
 import { memo } from 'react';
 import { useTheme } from '../context/Theme';
 
+const centerX = 100;
+const centerY = 100;
+const radius = 70;
+
+const getPosition = ({
+	index,
+	letterArray,
+}: {
+	index: number;
+	letterArray: string[];
+}): { x: number; y: number } => {
+	const angle = (360 / letterArray.length) * index;
+	const radian = (angle * Math.PI) / 180; // Convert degrees to radians
+	return {
+		x: centerX + radius * Math.cos(radian),
+		y: centerY + radius * Math.sin(radian),
+	};
+};
+
+const renderOuterLetters = ({
+	letterArray,
+	fill,
+}: {
+	letterArray: string[];
+	fill?: string;
+}): JSX.Element[] => {
+	return letterArray.map((letter, index) => {
+		const { x, y } = getPosition({ letterArray: letterArray, index });
+		return (
+			<text
+				key={index}
+				x={x}
+				y={y}
+				textAnchor="middle"
+				dominantBaseline="middle"
+				css={css`
+					${textSans17}
+				`}
+				fill={fill}
+			>
+				{letter}
+			</text>
+		);
+	});
+};
+
 const WordWheelComponent = ({
 	candidateLetters,
 }: {
@@ -15,42 +61,7 @@ const WordWheelComponent = ({
 		.split('')
 		.sort(() => Math.random() - 0.5);
 
-	const outerLetters = letterArray;
-	const centerLetter =
-		letterArray.length > 4 ? outerLetters.shift() : undefined;
-	const radius = 70;
-	const centerX = 100;
-	const centerY = 100;
-
-	const getPosition = (index: number): { x: number; y: number } => {
-		const angle = (360 / outerLetters.length) * index;
-		const radian = (angle * Math.PI) / 180; // Convert degrees to radians
-		return {
-			x: centerX + radius * Math.cos(radian),
-			y: centerY + radius * Math.sin(radian),
-		};
-	};
-
-	const renderOuterLetters = (): JSX.Element[] => {
-		return outerLetters.map((letter, index) => {
-			const { x, y } = getPosition(index);
-			return (
-				<text
-					key={index}
-					x={x}
-					y={y}
-					textAnchor="middle"
-					dominantBaseline="middle"
-					css={css`
-						${textSans17}
-					`}
-					fill={theme.text}
-				>
-					{letter}
-				</text>
-			);
-		});
-	};
+	const centerLetter = letterArray.length > 4 ? letterArray.shift() : undefined;
 
 	return (
 		<svg width="200" height="200">
@@ -68,7 +79,7 @@ const WordWheelComponent = ({
 					{centerLetter}
 				</text>
 			)}
-			{renderOuterLetters()}
+			{renderOuterLetters({ letterArray, fill: theme.text })}
 		</svg>
 	);
 };
