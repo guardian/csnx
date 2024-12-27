0770428a67e7daa7477eb5d15747a1371b761a2b
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-12-27 09:06:27 +0000

    Stop pointing to test SP environment

--- libs/@guardian/libs/src/consent-management-platform/exclusionList.ts
+++ libs/@guardian/libs/src/consent-management-platform/exclusionList.ts
@@ -1,6 +1,5 @@
 const sectionExclusionList = ['info', 'help', 'community', 'identity'];
 
 export const isExcludedFromCMP = (pageSection: string): boolean => {
-	console.log('pageSection', pageSection);
 	return sectionExclusionList.some((section) => section === pageSection);
 };
