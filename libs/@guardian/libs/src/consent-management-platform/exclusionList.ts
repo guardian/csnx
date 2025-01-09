8ce8ed907485bbea4917819890bf6eb9a700b995
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-01-09 12:22:02 +0000

    Revert "Temporarily point to test account for final test"
    This reverts commit f92b6645f32ff66c165f4eb913cdf7244a59e547.

--- libs/@guardian/libs/src/consent-management-platform/exclusionList.ts
+++ libs/@guardian/libs/src/consent-management-platform/exclusionList.ts
@@ -1,6 +1,5 @@
 const sectionExclusionList = ['info', 'help'];
 
 export const isExcludedFromCMP = (pageSection: string): boolean => {
-	console.log('pageSection', pageSection);
 	return sectionExclusionList.some((section) => section === pageSection);
 };
