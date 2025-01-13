6548445c05ab4c40e17726e02d4b6b209f44eecd
oliverabrahams <ollie.abrahams@guardian.co.uk>
2025-01-13 10:40:53 +0000

    remove forwarded ref from cell

--- libs/@guardian/react-crossword/src/components/Cell.tsx
+++ libs/@guardian/react-crossword/src/components/Cell.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 import { textSans12 } from '@guardian/source/foundations';
-import { forwardRef, memo } from 'react';
+import { memo } from 'react';
 import type { Cell as CellType } from '../@types/crossword';
 import { useTheme } from '../context/Theme';
 
@@ -16,100 +16,95 @@ export type CellProps = {
 	isSelected?: boolean;
 } & React.SVGProps<SVGGElement>;
 
-const CellComponent = forwardRef<SVGGElement, CellProps>(
-	(
-		{
-			data,
-			x,
-			y,
-			guess = '',
-			isBlackCell,
-			isConnected,
-			isSelected,
-			children,
-			...props
-		},
-		ref,
-	) => {
-		const theme = useTheme();
+const CellComponent = ({
+	data,
+	x,
+	y,
+	guess = '',
+	isBlackCell,
+	isConnected,
+	isSelected,
+	children,
+	...props
+}: CellProps) => {
+	const theme = useTheme();
 
-		const backgroundColor = isBlackCell
-			? 'transparent'
-			: isConnected
-				? isSelected
-					? theme.selectedColor
-					: theme.connectedColor
-				: theme.gridForegroundColor;
+	const backgroundColor = isBlackCell
+		? 'transparent'
+		: isConnected
+			? isSelected
+				? theme.selectedColor
+				: theme.connectedColor
+			: theme.gridForegroundColor;
 
-		return (
-			<g {...props} ref={ref}>
-				<rect
-					x={x}
-					y={y}
-					width={theme.gridCellSize}
-					height={theme.gridCellSize}
-					fill={backgroundColor}
-					aria-hidden="true"
-					role="presentation"
-				/>
-				{!isBlackCell && (
-					<>
-						{data.number && (
-							<text
-								x={x}
-								y={y}
-								dx={Math.max(1, theme.gridCellSize * 0.05)}
-								dy={Math.max(9, theme.gridCellSize * 0.22)}
-								fill={theme.textColor}
-								css={css`
-									${textSans12};
-									font-size: ${Math.max(
-										9,
-										Math.round(theme.gridCellSize * 0.2),
-									)}px;
-								`}
-								aria-hidden="true"
-								role="presentation"
-							>
-								{data.number}
-							</text>
-						)}
+	return (
+		<g {...props}>
+			<rect
+				x={x}
+				y={y}
+				width={theme.gridCellSize}
+				height={theme.gridCellSize}
+				fill={backgroundColor}
+				aria-hidden="true"
+				role="presentation"
+			/>
+			{!isBlackCell && (
+				<>
+					{data.number && (
+						<text
+							x={x}
+							y={y}
+							dx={Math.max(1, theme.gridCellSize * 0.05)}
+							dy={Math.max(9, theme.gridCellSize * 0.22)}
+							fill={theme.textColor}
+							css={css`
+								${textSans12};
+								font-size: ${Math.max(
+									9,
+									Math.round(theme.gridCellSize * 0.2),
+								)}px;
+							`}
+							aria-hidden="true"
+							role="presentation"
+						>
+							{data.number}
+						</text>
+					)}
 
-						{children ? (
-							<foreignObject
-								x={x}
-								y={y}
-								width={theme.gridCellSize}
-								height={theme.gridCellSize}
-								css={css`
-									position: relative;
-								`}
-							>
-								{children}
-							</foreignObject>
-						) : (
-							<text
-								x={x + theme.gridCellSize / 2}
-								y={y + theme.gridCellSize / 2}
-								dy={theme.gridCellSize * 0.07}
-								textAnchor="middle"
-								dominantBaseline="middle"
-								fill={theme.textColor}
-								css={css`
-									${textSans12};
-									font-size: ${theme.gridCellSize * 0.6}px;
-								`}
-								aria-hidden="true"
-								role="presentation"
-							>
-								{guess}
-							</text>
-						)}
-					</>
-				)}
-			</g>
-		);
-	},
-);
+					{children ? (
+						<foreignObject
+							x={x}
+							y={y}
+							width={theme.gridCellSize}
+							height={theme.gridCellSize}
+							css={css`
+								position: relative;
+							`}
+						>
+							{children}
+						</foreignObject>
+					) : (
+						<text
+							x={x + theme.gridCellSize / 2}
+							y={y + theme.gridCellSize / 2}
+							dy={theme.gridCellSize * 0.07}
+							textAnchor="middle"
+							dominantBaseline="middle"
+							fill={theme.textColor}
+							css={css`
+								${textSans12};
+								font-size: ${theme.gridCellSize * 0.6}px;
+							`}
+							aria-hidden="true"
+							role="presentation"
+						>
+							{guess}
+						</text>
+					)}
+				</>
+			)}
+		</g>
+	);
+};
 
 export const Cell = memo(CellComponent);
