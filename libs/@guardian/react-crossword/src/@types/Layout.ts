483a26b7a764abdfdaf61ebc3c8befe2f6da22c3
oliverabrahams <ollie.abrahams@guardian.co.uk>
2025-01-13 10:20:16 +0000

    remove .table

--- libs/@guardian/react-crossword/src/@types/Layout.ts
+++ libs/@guardian/react-crossword/src/@types/Layout.ts
@@ -2,7 +2,7 @@ import type { ComponentType } from 'react';
 import type { AnagramHelper } from '../components/AnagramHelper';
 import type { Clues } from '../components/Clues';
 import type { Controls } from '../components/Controls';
-import type { Grid } from '../components/Grid.table';
+import type { Grid } from '../components/Grid';
 
 export type LayoutProps = {
 	Grid: typeof Grid;
