0067a059d76d20dee497fe0a1ea940d9a4cf21a5
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-01-20 13:12:16 +0000

    Use isUserSignedIn and useNonAdvertisedList in CMP.init

--- libs/@guardian/libs/src/consent-management-platform/cmp.ts
+++ libs/@guardian/libs/src/consent-management-platform/cmp.ts
@@ -1,3 +1,4 @@
+import { CountryCode } from '../index.test';
 import { getCurrentFramework } from './getCurrentFramework';
 import { mark } from './lib/mark';
 import {
@@ -16,9 +17,21 @@ import type {
 	WillShowPrivacyMessage,
 } from './types';
 
-const init = (framework: ConsentFramework, pubData?: PubData): void => {
+const init = (
+	framework: ConsentFramework,
+	countryCode: CountryCode,
+	isUserSignedIn: boolean,
+	useNonAdvertisedList: boolean,
+	pubData?: PubData,
+): void => {
 	mark('cmp-init');
-	initSourcepoint(framework, pubData);
+	initSourcepoint(
+		framework,
+		countryCode,
+		isUserSignedIn,
+		useNonAdvertisedList,
+		pubData,
+	);
 };
 
 const willShowPrivacyMessage: WillShowPrivacyMessage = () =>
