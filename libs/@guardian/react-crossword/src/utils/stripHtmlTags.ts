792b3017d68e0bb9c7413e90dbcc44ff4379db7c
oliverabrahams <ollie.abrahams@guardian.co.uk>
2025-01-27 14:55:18 +0000

    back to regex

--- libs/@guardian/react-crossword/src/utils/stripHtmlTags.ts
+++ libs/@guardian/react-crossword/src/utils/stripHtmlTags.ts
@@ -3,6 +3,6 @@ export const stripHtmlTags = (html: string): string => {
 	// Remove the allowed tags for a clue: span, i, b, sup, sub
 	// Reference: https://crossword.info/docs/schema-doc.html#h464208646
 	// On the server, there is no DOMParser, so we have to use regex or install a html parsing library.
-	// As we know the tags that are allowed, this approach is sufficient.
+	// As we know the tags that are allowed, regex is sufficient.
 	return html.replace(regex, '');
 };
