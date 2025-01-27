b64db5caf5b99911721e1129f240951d9980c26d
oliverabrahams <ollie.abrahams@guardian.co.uk>
2025-01-27 14:43:20 +0000

    back to regex

--- libs/@guardian/react-crossword/src/utils/stripHtmlTags.ts
+++ libs/@guardian/react-crossword/src/utils/stripHtmlTags.ts
@@ -1,4 +1,8 @@
+const regex = /<\/?(span|i|b|sup|sub)[^>]*?>/g;
 export const stripHtmlTags = (html: string): string => {
-	const doc = new DOMParser().parseFromString(html, 'text/html');
-	return doc.body.textContent ?? '';
+	// Remove the allowed tags for a clue: span, i, b, sup, sub
+	// Reference: https://crossword.info/docs/schema-doc.html#h464208646
+	// On the server, there is no DOMParser, so we have to use regex.
+	// As we know the tags that are allowed, this approach is sufficient.
+	return html.replace(regex, '');
 };
