f846b5ccc7a7e8922720cb6939dbd3744877a373
Oliver Abrahams <ollie.abrahams@guardian.co.uk>
2025-02-12 16:33:15 +0000

    Add check to cell and a display to show if the cell is correct  (#1941)
    * Add check to cell and a display to show if the cell is correct with a strike through if not correct

--- libs/@guardian/react-crossword/src/hooks/useUpdateCell.ts
+++ libs/@guardian/react-crossword/src/hooks/useUpdateCell.ts
@@ -6,15 +6,16 @@ import { useProgress } from '../context/Progress';
 import { useValidAnswers } from '../context/ValidAnswers';
 
 export const useUpdateCell = () => {
-	const { updateProgress, progress } = useProgress();
-	const { setValidAnswers } = useValidAnswers();
+	const { progress, updateProgress } = useProgress();
+	const { setValidAnswers, invalidCellAnswers, setInvalidCellAnswers } =
+		useValidAnswers();
+
 	const { cells } = useData();
 
 	const updateCell = useCallback(
 		({ x, y, value }: Coords & { value: string }) => {
 			const cell = cells.getByCoords({ x, y });
 			const cellGroup = cell?.group;
-
 			// blank cells have no group
 			if (isUndefined(cellGroup)) {
 				return;
@@ -31,6 +32,14 @@ export const useUpdateCell = () => {
 			newProgress[x][y] = value;
 			updateProgress(newProgress);
 
+			if (invalidCellAnswers.has(`x${x}y${y}`)) {
+				setInvalidCellAnswers((prevState) => {
+					const newInvalidCellAnswers = new Set(prevState);
+					newInvalidCellAnswers.delete(`x${x}y${y}`);
+					return newInvalidCellAnswers;
+				});
+			}
+
 			setValidAnswers((prev) => {
 				const newSet = new Set(prev);
 				for (const entryId of cellGroup) {
@@ -39,7 +48,14 @@ export const useUpdateCell = () => {
 				return newSet;
 			});
 		},
-		[cells, progress, updateProgress, setValidAnswers],
+		[
+			cells,
+			invalidCellAnswers,
+			progress,
+			setInvalidCellAnswers,
+			setValidAnswers,
+			updateProgress,
+		],
 	);
 	return { updateCell };
 };
