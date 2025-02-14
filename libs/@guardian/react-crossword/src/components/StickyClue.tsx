ef2c0d16556504336dc8eb415ce886eb9d505718
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2025-02-14 18:06:26 +0000

    Move sticky clue to bottom of grid when out of view

--- libs/@guardian/react-crossword/src/components/StickyClue.tsx
+++ libs/@guardian/react-crossword/src/components/StickyClue.tsx
@@ -2,7 +2,7 @@ import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
 import { isUndefined } from '@guardian/libs';
 import { textSans12 } from '@guardian/source/foundations';
-import { memo } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 import { useCurrentClue } from '../context/CurrentClue';
 import { useData } from '../context/Data';
 import { useTheme } from '../context/Theme';
@@ -12,6 +12,8 @@ type StickyClueProps = {
 };
 
 export const StickyClueComponent = ({ additionalCss }: StickyClueProps) => {
+	const stickyClueRef = useRef<HTMLDivElement>(null);
+	const [stickyClueBottom, setStickyClueBottom] = useState(false);
 	const { entries } = useData();
 	const { currentEntryId } = useCurrentClue();
 	const theme = useTheme();
@@ -19,44 +21,80 @@ export const StickyClueComponent = ({ additionalCss }: StickyClueProps) => {
 		? entries.get(currentEntryId)
 		: undefined;
 
-	const stickyClue = css`
+	useEffect(() => {
+		const stickyClueElement = stickyClueRef.current;
+		if (!stickyClueElement) {
+			return;
+		}
+
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				if (entry) {
+					setStickyClueBottom(entry.intersectionRatio < 1);
+				}
+			},
+			{ threshold: [1] },
+		);
+
+		observer.observe(stickyClueRef.current);
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [stickyClueRef]);
+
+	const placeholder = css`
+		position: absolute;
 		top: 0;
-		position: sticky;
+		min-height: calc(0.75rem * 3.5);
+		width: 100%;
+	`;
+
+	const stickyClue = css`
+		position: absolute;
 		display: flex;
-		z-index: 1;
+		top: 0;
 		min-height: 3.5em;
 		align-items: start;
 		${textSans12};
 		background: ${theme.stickyClueBackgroundColour};
+		${stickyClueBottom &&
+		css`
+			top: auto;
+			bottom: 0;
+		`};
 		@media print {
 			display: none;
 		}
 	`;
 
 	return (
-		<div aria-hidden="true" css={[additionalCss, stickyClue]}>
-			{entry && (
-				<>
-					<span
-						aria-hidden="true"
-						css={css`
-							flex: 0 0 auto;
-							font-weight: bold;
-							padding-right: 0.625em;
-							text-transform: capitalize;
-						`}
-					>
-						{entry.id.split('-').join(' ')}
-					</span>
-					<span
-						aria-hidden="true"
-						dangerouslySetInnerHTML={{
-							__html: entry.clue,
-						}}
-					></span>
-				</>
-			)}
-		</div>
+		<>
+			<div css={placeholder} ref={stickyClueRef}></div>
+			<div aria-hidden="true" css={[stickyClue, additionalCss]}>
+				{entry && (
+					<>
+						<span
+							aria-hidden="true"
+							css={css`
+								flex: 0 0 auto;
+								font-weight: bold;
+								padding-right: 0.625em;
+								text-transform: capitalize;
+							`}
+						>
+							{entry.id.split('-').join(' ')}
+						</span>
+						<span
+							aria-hidden="true"
+							dangerouslySetInnerHTML={{
+								__html: entry.clue,
+							}}
+						></span>
+					</>
+				)}
+			</div>
+		</>
 	);
 };
 
