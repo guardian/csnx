1f2c5973b032edfd65be9c97f315e8fbdc1d6ba9
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-02-14 12:19:20 +0000

    Remove unused ab-test function

--- libs/@guardian/libs/src/consent-management-platform/isConsentOrPay.ts
+++ libs/@guardian/libs/src/consent-management-platform/isConsentOrPay.ts
@@ -1,9 +1,6 @@
 import type { CountryCode } from '../index.test';
-import { isObject } from '../isObject/isObject';
-import { storage } from '../storage/storage';
 import { isGuardianDomain } from './lib/domain';
 import { consentOrPayCountries } from './lib/sourcepointConfig';
-import type { Participations } from './types';
 
 let _isConsentOrPay = false;
 
@@ -17,15 +14,6 @@ export const getIsConsentOrPay = (): boolean => {
 
 export const isConsentOrPayCountry = (countryCode: CountryCode) => {
 	return consentOrPayCountries.includes(countryCode);
-};
-
-export const isInConsentOrPayABTest = (): boolean => {
-	const participations: Participations = storage.local.get(
-		'gu.ab.participations',
-	) as Participations;
-	return isObject(participations)
-		? participations.ConsentOrPayBanner?.variant === 'activate'
-		: false;
 };
 
 export const getSupportSignUpPage = (): string => {
