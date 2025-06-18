b5574e97caf5687af72afeae0c14b79e1ec54301
Akinsola Lawanson <akinsola.lawanson@guardian.co.uk>
2025-06-18 11:20:56 +0100

    Add reject to promise and check consent regardless of applies value

--- libs/@guardian/libs/src/consent-management-platform/usnat/api.ts
+++ libs/@guardian/libs/src/consent-management-platform/usnat/api.ts
@@ -1,11 +1,15 @@
 import type { UsnatData } from '../types/usnat';
 
 export const api = () =>
-	new Promise((resolve) => {
+	new Promise((resolve, reject) => {
 		if (window._sp_?.usnat?.getUserConsents) {
 			window._sp_.usnat.getUserConsents((usNatData) => {
 				return resolve({ ...usNatData, signalStatus: 'ready' });
 			});
+		} else {
+			return reject(
+				new Error('window._sp_.usnat.getUserConsents is not available.'),
+			);
 		}
 	});
 
