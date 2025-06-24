5d4232024b930afb4e380f7f4bca5c5d20a7c0f9
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-06-24 09:48:11 +0100

    Migrate from GPP to Sourcepoint API (#2096)
    * Update types to include new function and inputs
    
    * Update Usnat api and getConsentState function
    
    * Fix tests
    
    * Add changeset
    
    * Remove tests for old framework
    
    * Add reject to promise and check consent regardless of applies value
    
    * Revert "Add reject to promise and check consent regardless of applies value"
    
    This reverts commit b5574e97caf5687af72afeae0c14b79e1ec54301.
    
    * Resolvoing promise if window._sp_.usnat.getUserConsents doesn't exits
    
    * Remove applies check.
    
    * Make categories and vendors conditional and check if consented is false

--- libs/@guardian/libs/src/consent-management-platform/usnat/api.ts
+++ libs/@guardian/libs/src/consent-management-platform/usnat/api.ts
@@ -1,19 +1,20 @@
-import type { GPPData } from '../types/usnat';
+import type { UsnatData } from '../types/usnat';
 
-type Command = 'ping';
-
-const api = (command: Command) =>
-	new Promise((resolve, reject) => {
-		if (window.__gpp) {
-			window.__gpp(command, (result, success) =>
-				success
-					? resolve(result)
-					: /* istanbul ignore next */
-						reject(new Error(`Unable to get ${command} data`)),
-			);
+export const api = () =>
+	new Promise((resolve) => {
+		if (window._sp_?.usnat?.getUserConsents) {
+			window._sp_.usnat.getUserConsents((usNatData) => {
+				return resolve({ ...usNatData, signalStatus: 'ready' });
+			});
 		} else {
-			reject(new Error('No __gpp found on window'));
+			resolve({
+				applies: false,
+				categories: [],
+				vendors: [],
+				signalStatus: 'not ready',
+			});
 		}
 	});
-export const getGPPData = (): Promise<GPPData> =>
-	api('ping') as Promise<GPPData>;
+
+export const getUsnatData = (): Promise<UsnatData> =>
+	api() as Promise<UsnatData>;
