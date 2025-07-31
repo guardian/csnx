e9343ae2469663ad06871c80135a4af222763efa
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2025-07-31 11:31:06 +0100

    delete test and related packages
    - will move to playwright

--- libs/@guardian/stand/jest.config.js
+++ libs/@guardian/stand/jest.config.js
@@ -3,9 +3,7 @@ import { config as baseConfig } from '../../../configs/jest.config.js';
 /** @typedef {import("jest").Config} Config  */
 const config = {
 	...baseConfig,
-	testEnvironment: 'jsdom',
 	displayName: '@guardian/stand',
-	setupFilesAfterEnv: ['<rootDir>/jest-setup-after-env.ts'],
 };
 
 export default config;
