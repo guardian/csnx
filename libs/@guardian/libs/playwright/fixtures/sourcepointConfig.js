0822ae17bb5fafceb3354bca5deb000c2a128207
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2025-08-12 12:24:17 +0100

    Update sourcepointConfig fixture

--- libs/@guardian/libs/playwright/fixtures/sourcepointConfig.js
+++ libs/@guardian/libs/playwright/fixtures/sourcepointConfig.js
@@ -1,5 +1,4 @@
 export const ACCOUNT_ID = 1257;
-export const PRIVACY_MANAGER_AUSTRALIA = 1325716;
+export const PRIVACY_MANAGER_AUSTRALIA = 1349649;
 export const PRIVACY_MANAGER_USNAT = 1068329;
-export const PRIVACY_MANAGER_USNAT_SECOND_LAYER = 1129533;
 export const ENDPOINT = 'https://cdn.privacy-mgmt.com';
