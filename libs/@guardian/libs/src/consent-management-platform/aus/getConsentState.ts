76050d4b196a09033a1107aff293e4fc82ec773f
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2025-08-14 08:48:19 +0100

    Remove redundant check

--- libs/@guardian/libs/src/consent-management-platform/aus/getConsentState.ts
+++ libs/@guardian/libs/src/consent-management-platform/aus/getConsentState.ts
@@ -1,19 +1,9 @@
-import { log } from '../../logger/logger';
 import { PRIVACY_CHOICE_ID_AUSTRALIA } from '../lib/sourcepointConfig';
 import type { AUSConsentState } from '../types/aus';
 import { getGlobalEnterpriseConsents } from './api';
 
 export const getConsentState: () => Promise<AUSConsentState> = async () => {
 	const globalEnterpriseData = await getGlobalEnterpriseConsents();
-
-	// If the CMP doesn't apply to this user, assume no consent
-	if (!globalEnterpriseData.applies) {
-		log('cmp', `Global Enterprise CMP does not apply to this user`);
-		return {
-			personalisedAdvertising: false,
-			signalStatus: 'not ready',
-		};
-	}
 
 	const personalizedAdCategory = globalEnterpriseData.categories.find(
 		(category) => category._id === PRIVACY_CHOICE_ID_AUSTRALIA,
