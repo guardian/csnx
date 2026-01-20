b05cd7c3a9d476e975542a031d0410f2b9c4bf99
AshCorr <ashcorr20@gmail.com>
2026-01-20 12:49:44 +0000

    fix: ERR_MODULE_NOT_FOUND when loading @guardian/eslint-config without Storybook

---
+++ .changeset/afraid-paths-rule.md
@@ -0,0 +1,5 @@
+---
+'@guardian/eslint-config': patch
+---
+
+Fix ERR_MODULE_NOT_FOUND when importing @guardian/eslint-config in a project that does not have Storybook.
--- libs/@guardian/eslint-config/configs/storybook.js
+++ libs/@guardian/eslint-config/configs/storybook.js
@@ -1,9 +1,19 @@
-import storybook from 'eslint-plugin-storybook';
+// eslint-plugin-storybook has a dependency on on Storybook which causes it
+// to crash eslint when it is imported if Storybook is not installed.
+const loadStorybookRecommended = async () => {
+	// eslint-disable-next-line import/no-unresolved -- We're checking if Storybook is resolveable
+	if (await import('storybook').then(() => true).catch(() => false)) {
+		return [];
+	}
+
+	const storybook = await import('eslint-plugin-storybook');
+	return storybook.configs['flat/recommended'];
+};
 
 export default [
 	{
 		name: '@guardian/storybook',
 		files: ['**/*.stories.{js,mjs,cjs,jsx,mjsx,ts,mts,cts,tsx,mtsx,mdx}'],
 	},
-	...storybook.configs['flat/recommended'],
+	...(await loadStorybookRecommended()),
 ];
