c267f870ef9cb21d794f717a144ca06e7c405dc1
Mahesh Makani <mahesh@makani.dev>
2026-03-02 10:29:42 +0000

    chore(eslint-config): remove `storybook` as transitive dep

--- libs/@guardian/eslint-config/configs/storybook.js
+++ libs/@guardian/eslint-config/configs/storybook.js
@@ -1,15 +1,16 @@
-// eslint-plugin-storybook has a dependency on on Storybook which causes it
-// to crash eslint when it is imported if Storybook is not installed.
+// eslint-plugin-storybook is an optional peer dependency.
+// It also has its own peer dependency on Storybook which can cause it
+// to crash eslint when imported if Storybook is not installed.
 const loadStorybookRecommended = async () => {
 	try {
-		// eslint-disable-next-line import/no-unresolved -- We're checking if Storybook is resolveable
+		// eslint-disable-next-line import/no-unresolved -- We're checking if Storybook is resolvable
 		await import('storybook');
+		// Check that plugin is also available
+		const storybook = await import('eslint-plugin-storybook');
+		return storybook.configs['flat/recommended'];
 	} catch {
 		return [];
 	}
-
-	const storybook = await import('eslint-plugin-storybook');
-	return storybook.configs['flat/recommended'];
 };
 
 export default [
