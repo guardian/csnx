ec534775a0ab38a6fbe4e7f9688c17bcd817cbde
Mahesh Makani <mahesh@makani.dev>
2026-03-02 10:29:42 +0000

    chore(eslint-config): remove `storybook` as transitive dep

---
+++ .changeset/thin-adults-yawn.md
@@ -0,0 +1,5 @@
+---
+'@guardian/eslint-config': major
+---
+
+Make `eslint-plugin-storybook` into an optional peer dependency
--- libs/@guardian/eslint-config/configs/storybook.js
+++ libs/@guardian/eslint-config/configs/storybook.js
@@ -1,7 +1,10 @@
-// eslint-plugin-storybook has a dependency on on Storybook which causes it
-// to crash eslint when it is imported if Storybook is not installed.
+// eslint-plugin-storybook is an optional peer dependency.
+// It also has its own peer dependency on Storybook which can cause it
+// to crash eslint when imported if Storybook is not installed.
 const loadStorybookRecommended = async () => {
 	try {
+		// Check that both the plugin and storybook itself are available
+		await import('eslint-plugin-storybook');
 		// eslint-disable-next-line import/no-unresolved -- We're checking if Storybook is resolveable
 		await import('storybook');
 	} catch {
