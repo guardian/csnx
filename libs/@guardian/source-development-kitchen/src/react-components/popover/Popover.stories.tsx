0eafde11fc92b322c7dc1c266b7d162043109fa4
Charlotte <charlotte.emms@theguardian.com>
2026-05-11 16:14:25 +0100

    fix positioning of popover + pointer

--- libs/@guardian/source-development-kitchen/src/react-components/popover/Popover.stories.tsx
+++ libs/@guardian/source-development-kitchen/src/react-components/popover/Popover.stories.tsx
@@ -1,45 +1,76 @@
 import { css } from '@emotion/react';
-import { SvgInfoRound } from '@guardian/source/react-components';
+import { space } from '@guardian/source/foundations';
+import {
+	Button,
+	LinkButton,
+	SvgInfoRound,
+} from '@guardian/source/react-components';
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { fn } from 'storybook/test';
+import { useState } from 'react';
 import { Popover } from './Popover';
 
 const meta: Meta<typeof Popover> = {
 	title: 'React Components/Popover',
 	component: Popover,
 	args: {
-		content: (
-			<span>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua.
-			</span>
-		),
-		onDismiss: fn(),
 		title: 'Title',
-		ctaButtonText: 'Primary button xs',
-		ctaButtonOnClick: fn(),
 		position: 'top',
 		showPointer: true,
-		refButtonOverrides: {
-			icon: <SvgInfoRound />,
-			size: 'xsmall',
-			priority: 'tertiary',
-			hideLabel: true,
-		},
-		width: 200,
+		theme: 'medium',
 	},
-	render: (args) => (
-		<div
-			css={css`
-				position: relative;
-				width: fit-content;
-				left: 300px;
-				top: 300px;
-			`}
-		>
-			<Popover {...args} theme="medium" />
-		</div>
-	),
+	render: (args) => {
+		// eslint-disable-next-line react-hooks/rules-of-hooks -- Storybook
+		const [isExpanded, setIsExpanded] = useState(false);
+		const handleButtonClick = () => {
+			console.log('isExpanded?', isExpanded);
+			setIsExpanded(!isExpanded);
+		};
+
+		return (
+			<div
+				css={css`
+					position: relative;
+					width: fit-content;
+					left: 300px;
+					top: 300px;
+				`}
+			>
+				<Popover
+					{...args}
+					target={
+						<Button
+							icon={<SvgInfoRound />}
+							size="xsmall"
+							priority="tertiary"
+							theme={{ borderTertiary: 'unset' }}
+							hideLabel={true}
+							onClick={handleButtonClick}
+						/>
+					}
+					isOpen={isExpanded}
+					handleClose={() => setIsExpanded(false)}
+				>
+					<span>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</span>
+					<div
+						css={css`
+							margin-top: ${space[3]}px;
+						`}
+					>
+						<LinkButton
+							priority="primary"
+							size="xsmall"
+							href="https://www.theguardian.com/uk"
+						>
+							Primary button xs
+						</LinkButton>
+					</div>
+				</Popover>
+			</div>
+		);
+	},
 };
 
 export default meta;
@@ -50,11 +81,5 @@ export const WithTitle: Story = {};
 export const WithoutTitle: Story = {
 	args: {
 		title: undefined,
-	},
-};
-
-export const WithoutButton: Story = {
-	args: {
-		ctaButtonText: undefined,
 	},
 };
