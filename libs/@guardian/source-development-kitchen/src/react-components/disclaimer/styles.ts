a92d9f29ce7b332e5b6609d0909cb4e99a5c102c
Solomon Owen-Williams <solomon.owen-williams@guardian.co.uk>
2026-07-30 16:13:51 +0100

    use correct space

--- libs/@guardian/source-development-kitchen/src/react-components/disclaimer/styles.ts
+++ libs/@guardian/source-development-kitchen/src/react-components/disclaimer/styles.ts
@@ -1,6 +1,6 @@
 import type { SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react';
-import { textSans14 } from '@guardian/source/foundations';
+import { space, textSans14 } from '@guardian/source/foundations';
 import { defaultDisclaimerTheme, type DisclaimerTheme } from './theme';
 
 export const disclaimerStyles = (
@@ -10,7 +10,7 @@ export const disclaimerStyles = (
 	return css`
 		${textSans14};
 		width: 100%;
-		padding: 8px 10px 10px 10px;
+		padding: ${space[2]}px 10px 10px 10px;
 		background-color: ${theme.backgroundPrimary};
 		color: ${theme.textPrimary};
 
