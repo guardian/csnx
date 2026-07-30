c901930290a9d5f5a8ced1330eddf04f9493a27c
Solomon Owen-Williams <solomon.owen-williams@guardian.co.uk>
2026-07-30 15:28:19 +0100

    update disclaimer description

--- libs/@guardian/source-development-kitchen/src/react-components/disclaimer/Disclaimer.tsx
+++ libs/@guardian/source-development-kitchen/src/react-components/disclaimer/Disclaimer.tsx
@@ -4,11 +4,7 @@ import { disclaimerStyles } from './styles';
 import { type DisclaimerTheme } from './theme';
 
 /**
- * [GitHub](https://github.com/guardian/csnx/tree/main/libs/@guardian/source/src/react-components/disclaimer/Disclaimer.tsx) •
- * [NPM](https://www.npmjs.com/package/@guardian/source)
- *
  * Disclaimer is used to disclose limitations, risks, non-liability, or conditions of use associated with the surrounding material.
- *
  */
 export const Disclaimer = ({
 	cssOverrides,
