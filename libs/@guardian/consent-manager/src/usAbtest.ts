9e7e918a1579002d2cf9220346827888695bde46
Akinsola Lawanson <akinsola.lawanson@guardian.co.uk>
2026-08-25 08:14:55 +0100

    Add US States

--- libs/@guardian/consent-manager/src/usAbtest.ts
+++ libs/@guardian/consent-manager/src/usAbtest.ts
@@ -3,7 +3,20 @@ import { getCookie } from '@guardian/libs';
 const AB_TEST_COOKIE_NAME = 'gu_client_ab_tests';
 const AB_TEST_GROUP_PREFIX = 'identity-and-trust-consent-rr-banner-us';
 const AB_TEST_GEO_REGION_COOKIE = 'GU_geo_country_region';
-const AB_TEST_US_STATES = ['CA']; // Add more US states here if needed
+const AB_TEST_US_STATES = [
+	'WA', // Washington
+	'NC', // North Carolina
+	'OH', // Ohio
+	'SC', // South Carolina
+	'MI', // Michigan
+	'AZ', // Arizona
+	'MO', // Missouri
+	'WI', // Wisconsin
+	'DC', // District of Columbia
+	'KS', // Kansas
+	'NM', // New Mexico
+	'ME', // Maine
+];
 
 export const isInUsStateForAbTest = (): boolean => {
 	const usStateCookie = getCookie({
