9464eaa4dce549348b281e50d3b3936640463579
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-09-04 18:23:56 +0100

    Export Pulse CSS as Emotion serialised styles

--- libs/@guardian/source/src/react-components/core-button/CoreButton.tsx
+++ libs/@guardian/source/src/react-components/core-button/CoreButton.tsx
@@ -1,14 +1,19 @@
-import '@guardian/pulse/pulse.css';
+import { Global } from '@emotion/react';
 import type { ButtonHTMLAttributes } from 'react';
+import { pulse } from '../../foundations';
 import { button } from './styles';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type -- prototype component has no additional props yet
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 /**
  * Buttons enable users to make choices or perform actions.
  */
 export const CoreButton = ({ children, ...props }: ButtonProps) => (
-	<button css={button} {...props}>
-		{children}
-	</button>
+	<>
+		<Global styles={pulse} />
+		<button css={button} {...props}>
+			{children}
+		</button>
+	</>
 );
