5e44dfaa4d968d145fb90c803696db8adffe19f2
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2026-09-04 15:39:09 +0100

    Relocate styles

--- libs/@guardian/source/src/react-components/core-button/CoreButton.tsx
+++ libs/@guardian/source/src/react-components/core-button/CoreButton.tsx
@@ -1,34 +1,6 @@
-import { css } from '@emotion/react';
 import '@guardian/pulse/pulse.css';
 import type { ButtonHTMLAttributes } from 'react';
-import { focusHaloSpaced } from '../../foundations';
-
-const styles = css`
-	box-sizing: border-box;
-	display: inline-flex;
-	justify-content: center;
-	align-items: center;
-	appearance: none;
-	color: var(--button-color-text);
-	background-color: var(--button-color-fill);
-	font: var(--button-label);
-	letter-spacing: var(--button-letter-spacing);
-	padding: var(--button-padding-vertical) var(--button-padding-horizontal);
-	border-radius: var(--button-radius);
-	border: none;
-	cursor: pointer;
-	text-decoration: none;
-	white-space: nowrap;
-	vertical-align: middle;
-
-	:disabled {
-		cursor: not-allowed;
-	}
-
-	&:focus {
-		${focusHaloSpaced};
-	}
-`;
+import { button } from './styles';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
@@ -36,7 +8,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
  * Buttons enable users to make choices or perform actions.
  */
 export const CoreButton = ({ children, ...props }: ButtonProps) => (
-	<button css={styles} {...props}>
+	<button css={button} {...props}>
 		{children}
 	</button>
 );
